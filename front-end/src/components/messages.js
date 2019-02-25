5fc275d68dd91635edbeee7efc9a0c87d4374335
Stephen983 <46939895+Stephen983@users.noreply.github.com>
2019-02-24 21:54:31 -0800

    Make const variables capitalized

--- front-end/src/components/messages.js
+++ front-end/src/components/messages.js
@@ -1,11 +1,11 @@
 export const messages = {
-    from_email: 'from_email_address=claymore@claymoreusa.com',
-    to_email: 'to_email_address=',
-    signup_email_subject: 'email_subject=Welcome to Claymore',
-    signup_email_content: 'email_content=You have successfully set up your new email account:',
-    change_email_subject: 'email_subject=Request of changing email address',
-    change_email_content: 'email_content=Your new Email Address is: ',
-    username_error_message: 'Username already exists',
-    email_error_message: 'Email already exists'
+    FROM_EMAIL: 'from_email_address=claymore@claymoreusa.com',
+    TO_EMAIL: 'to_email_address=',
+    SIGNUP_EMAIL_SUBJECT:   'email_subject=Welcome to Claymore',
+    SIGNUP_EMAIL_CONTENT:   'email_content=You have successfully set up your new email account:',
+    CHANGE_EMAIL_SUBJECT:   'email_subject=Request of changing email address',
+    CHANGE_EMAIL_CONTENT:   'email_content=Your new Email Address is: ',
+    USERNAME_ERROE_MESSAGE: 'Username already exists',
+    EMAIL_ERROR_MESSAGE:    'Email already exists'
 }
 
