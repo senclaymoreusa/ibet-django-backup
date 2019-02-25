21ab199fc0eeedd60bab98bd659fb0e9e074f2e9
Stephen983 <46939895+Stephen983@users.noreply.github.com>
2019-02-24 21:03:43 -0800

    Update messages.js

--- front-end/src/components/messages.js
+++ front-end/src/components/messages.js
@@ -1,7 +1,6 @@
 export const messages = {
     from_email: 'from_email_address=claymore@claymoreusa.com',
     to_email: 'to_email_address=',
-    email_subject: 'email_subject=',
     signup_email_subject: 'email_subject=Welcome to Claymore',
     signup_email_content: 'email_content=You have successfully set up your new email account:',
     change_email_subject: 'email_subject=Request of changing email address',
