96ab6045b28aa6ac3825be0ccdb3bd6fe6be5dbf
VickyClaymore <vicky@claymoreusa.com>
2019-04-24 21:49:36 -0700

    set user tag to be unique

--- apps/users/models.py
+++ apps/users/models.py
@@ -62,7 +62,7 @@ class MyUserManager(BaseUserManager):
 
 class UserTag(models.Model):
     tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50, verbose_name=_("Tag name"))
+    name = models.CharField(max_length=50, verbose_name=_("Tag name"), unique=True)
     name_zh = models.CharField(max_length=50, null=True, blank=True)
     name_fr = models.CharField(max_length=50, null=True, blank=True)
     notes = models.CharField(max_length=200, verbose_name=_("Notes"))
