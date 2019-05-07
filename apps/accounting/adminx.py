81b7a2f43a82b578a013240a92d134eb31a78fc9
wluuuu0801 <lu@claymoreusa.com>
2019-05-07 15:13:01 -0700

    build transaction and third party model

--- apps/accounting/adminx.py
+++ apps/accounting/adminx.py
@@ -1,8 +1,13 @@
 import xadmin
 
-from .models import Transaction
+from .models import Transaction, ThirdParty
 
 class TransactionAdmin(object):
     list_display = ('user_id', 'transaction_type', 'amount', 'status', 'channel', 'request_time')
+    
+class ThirdPartyAdmin(object):
+    list_display = ('thridParty_name', 'min_amount', 'max_amount', 'transaction_fee', 'currency', 'switch',)
+
 
 xadmin.site.register(Transaction,TransactionAdmin)
+xadmin.site.register(ThirdParty,ThirdPartyAdmin)
--- apps/accounting/models.py
+++ apps/accounting/models.py
@@ -6,11 +6,20 @@ from django.utils.translation import ugettext_lazy as _
 
 import uuid
 # Create your models here.
-
+CHANNEL_CHOICES = (
+    (0, 'Alipay'),
+    (1, 'Wechat'),
+    (2, 'Card'),
+)
+CURRENCY_CHOICES = (
+    (0, 'CNY'),
+    (1, 'USD'),
+    (2, 'PHP'),
+)
 class Transaction(models.Model):
     transaction_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name=_('User'))
-    amount = models.DecimalField(max_digits=19, decimal_places=2, verbose_name=_('Amount'))
+    amount = models.DecimalField(max_digits=20, decimal_places=2, verbose_name=_('Amount'))
     request_time = models.DateTimeField(default=timezone.now, verbose_name=_('Request Time'))
     arrive_time = models.DateTimeField(default=timezone.now, verbose_name=_('Arrive Time'))
 
@@ -20,11 +29,7 @@ class Transaction(models.Model):
     )
     status = models.SmallIntegerField(choices=STATE_CHOICES,default=1)
 
-    CHANNEL_CHOICES = (
-        (0, 'Alipay'),
-        (1, 'Wechat'),
-        (2, 'Card'),
-    )
+    
     channel = models.SmallIntegerField(choices=CHANNEL_CHOICES, default=2, verbose_name=_('Channel'))
 
     TYPE_CHOICES = (
@@ -41,3 +46,18 @@ class Transaction(models.Model):
         verbose_name = 'Transaction'
         verbose_name_plural = verbose_name
 
+class ThirdParty(models.Model):
+    thridParty_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    thridParty_name = models.SmallIntegerField(choices=CHANNEL_CHOICES, default=2, verbose_name=_('Name'))
+    currency = models.SmallIntegerField(choices=CURRENCY_CHOICES, default=0, verbose_name=_('Currency'))
+    min_amount = models.DecimalField(max_digits=20, decimal_places=2, default=0, verbose_name=_('Min Amount'))
+    max_amount = models.DecimalField(max_digits=20, decimal_places=2, default=0, verbose_name=_('Max Amount'))
+    transaction_fee = models.DecimalField(max_digits=20, decimal_places=2, default=0, blank=True, verbose_name=_('Transaction Fee'))
+    switch = models.BooleanField(default=True, verbose_name=_('Active'))
+
+    def __str__(self):
+        return '{0}'.format(self.thridParty_id)
+
+    class Meta:
+        verbose_name = 'ThirdParty'
+        verbose_name_plural = verbose_name
