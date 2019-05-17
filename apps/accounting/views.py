385ac7a9c4b4da5e8f277cb6f0cdd81625def57f
Angela <angela@claymoreusa.com>
2019-05-17 13:11:56 -0700

    add authentications to methods & save data to db

--- apps/accounting/views.py
+++ apps/accounting/views.py
@@ -39,21 +39,20 @@ def generateHash(key, message):
     #hash.hexdigest()
     return hash.hexdigest()
 
-#@permission_classes((IsAuthenticated, ))
-
 class getDepositMethod(generics.RetrieveUpdateDestroyAPIView):
     lookup_filed = 'pk'  #id
     queryset = DepositChannel.objects.all()
     serializer_class = depositMethodSerialize
+    permission_classes = (IsAuthenticated,)
     
     def get(self, request, *args, **kwargs):
-        queryset = DepositChannel.objects.all()
+        
         serializer = depositMethodSerialize(queryset)
 
         url = api + apiVersion +'/' + merchantId + deposit_url + currency + '/methods'
         headers = {'Accept': 'application/json'}
-        username = self.request.GET.get('username')
-        userId = CustomUser.objects.filter(username=username)
+        # username = self.request.GET.get('username')
+        # userId = CustomUser.objects.filter(username=username)
         
         message = bytes(merchantId + '|' + currency, 'utf-8')
         secret = bytes(merchantApiKey, 'utf-8')
@@ -64,7 +63,7 @@ class getDepositMethod(generics.RetrieveUpdateDestroyAPIView):
         for x in range(3):
             try:
                 r = requests.get(url, headers=headers, params = {
-                    'userId' : userId,
+                    # 'userId' : userId,
                     'hmac' : my_hmac,
                     'deviceType' : 'PC',
                 })
@@ -82,7 +81,7 @@ class getDepositMethod(generics.RetrieveUpdateDestroyAPIView):
         
         for x in data:
             
-            action = DepositChannel.objects.get_or_create(
+            create = DepositChannel.objects.get_or_create(
             thridParty_name= 3,
             method=x['method'],
             currency=3,
@@ -90,25 +89,18 @@ class getDepositMethod(generics.RetrieveUpdateDestroyAPIView):
             max_amount=x['limits'].get('maxTransactionAmount'),
             
         )
-        
-        
         return Response(data)
 
-
-   
-
-    
-
-
 class getBankList(generics.RetrieveUpdateDestroyAPIView):
-    queryset = CustomUser.objects.all()
+    queryset = DepositChannel.objects.all()
     serializer_class = bankListSerialize
-    
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs):
         url = api + apiVersion +'/' + merchantId + deposit_url +currency + '/methods/' + method + '/banks'
         headers = {'Accept': 'application/json'}
-        username = self.request.GET.get('username')
-        userId = CustomUser.objects.filter(username=username)
+        # username = self.request.GET.get('username')
+        # userId = CustomUser.objects.filter(username=username)
         message = bytes(merchantId + '|' + currency, 'utf-8')
         secret = bytes(merchantApiKey, 'utf-8')
         my_hmac = generateHash(secret, message)
@@ -118,7 +110,7 @@ class getBankList(generics.RetrieveUpdateDestroyAPIView):
         for x in range(3):
             try:
                 r = requests.get(url, headers=headers, params = {
-                    'userId' : userId,
+                    # 'userId' : userId,
                     'hmac' : my_hmac,
                     'deviceType' : 'PC',
                 })
@@ -138,15 +130,19 @@ class getBankList(generics.RetrieveUpdateDestroyAPIView):
         return Response(data)
 
 class getBankLimits(generics.RetrieveUpdateDestroyAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = bankLimitsSerialize
+    queryset = DepositChannel.objects.all()
+    serializer_class = depositMethodSerialize
+    permission_classes = (IsAuthenticated,)
     
     def get(self, request, *args, **kwargs):
+        queryset = DepositChannel.objects.all()
+        serializer = depositMethodSerialize(queryset)
         bank = 'CMBCCN'
+        
         url =  api + apiVersion +'/' + merchantId + deposit_url + currency + '/methods/' + method + '/banks/' + bank + '/limits'
         headers = {'Accept': 'application/json'}
-        username = self.request.GET.get('username')
-        userId = CustomUser.objects.filter(username=username)
+        # username = self.request.GET.get('username')
+        # userId = CustomUser.objects.filter(username=username)
         message = bytes(merchantId + '|' + currency, 'utf-8')
         secret = bytes(merchantApiKey, 'utf-8')
         my_hmac = generateHash(secret, message)
@@ -156,7 +152,7 @@ class getBankLimits(generics.RetrieveUpdateDestroyAPIView):
         for x in range(3):
             try:
                 r = requests.get(url, headers=headers, params = {
-                    'userId' : userId,
+                    # 'userId' : userId,
                     'hmac' : my_hmac,
                     'deviceType' : 'PC',
                 })
@@ -168,11 +164,24 @@ class getBankLimits(generics.RetrieveUpdateDestroyAPIView):
         if not success:
             logger.info('Failed to complete a request for...')
         # Handle error
-
-        
+    
         data = r.json()
         #print (my_hmac)
-        
+
+
+        for x in DepositChannel._meta.get_field('currency').choices:
+
+            if data['currency'] == x[1]:
+                cur_val = x[0]
+            
+        create = DepositChannel.objects.get_or_create(
+            thridParty_name= 3,
+            method= method,
+            currency= cur_val,
+            min_amount=data['minTransactionAmount'],
+            max_amount=data['maxTransactionAmount'],
+            
+        )
         return Response(data)
             
        
