1824a613e4bab66f5a00a5b1412d76ff5ce4b01a
Angela <angela@claymoreusa.com>
2019-05-21 15:51:28 -0700

    add submit deposit

--- apps/accounting/views.py
+++ apps/accounting/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View, generic
+from django.utils import timezone
 from users.models import Game, CustomUser, Category, Config, NoticeMessage
 from .models import Transaction, ThirdParty, DepositChannel, WithdrawChannel, DepositAccessManagement, WithdrawAccessManagement
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView, GenericAPIView, RetrieveUpdateAPIView
@@ -15,14 +16,15 @@ from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
 
 
-from .serializers import depositMethodSerialize, bankListSerialize,bankLimitsSerialize
+from .serializers import depositMethodSerialize, bankListSerialize,bankLimitsSerialize,submitDepositSerialize
 from django.conf import settings
 import requests,json
 import os
-
+import datetime,time
 import hmac
 import hashlib 
 import base64
+import logging
 
 #payment
 merchantId = settings.MERCHANTID
@@ -33,7 +35,7 @@ method = settings.METHOD
 api = settings.QAICASH_URL 
 deposit_url = settings.DEPOSIT_URL
 
-
+logger = logging.getLogger('django')
 
 def generateHash(key, message):
     hash = hmac.new(key, msg=message, digestmod=hashlib.sha256)
@@ -41,7 +43,6 @@ def generateHash(key, message):
     return hash.hexdigest()
 
 class getDepositMethod(generics.RetrieveUpdateDestroyAPIView):
-    lookup_filed = 'pk'  #id
     queryset = DepositChannel.objects.all()
     serializer_class = depositMethodSerialize
     permission_classes = (IsAuthenticated,)
@@ -193,5 +194,91 @@ class getBankLimits(generics.RetrieveUpdateDestroyAPIView):
             
         )
         return Response(data)
+
+class submitDeposit(generics.GenericAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = submitDepositSerialize
+    permission_classes = [AllowAny, ]
+    
+    def get_response(self,request):
+        serializer_class = self.get_response_serializer()
+        serializer = serializer_class(instance=data,
+                                          context={'request': self.request})
+        if serializer.is_valid():
+            serializer.save() 
             
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, *args, **kwargs):
+
+        serializer = submitDepositSerialize(self.queryset, many=True)
+        url =  api + apiVersion + '/' + merchantId + '/deposit/'
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        print(url)
+        dateTime = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).strftime('%Y%m%dT%H%M%S%z')
+        
+        orderId = self.request.POST.get('order_id')
+        amount =self.request.POST.get('amount')
+        language = self.request.POST.get('language')
+        userId = self.request.POST.get('user_id')
+        mymethod = self.request.POST.get('method')
+        list = [merchantId, orderId, amount, currency, dateTime, userId, method]
+        message = '|'.join(str(x) for x in list)
+        
+        mymessage = bytes(message, 'utf-8')
+        secret = bytes(merchantApiKey, 'utf-8')
+        my_hmac = generateHash(secret, mymessage)
+        delay = kwargs.get("delay", 5)
+        
+        first_name = self.request.GET.get('first_name')
+        email = self.request.GET.get('email')
+         #retry
+        success = False
+        r = requests.post(url, headers=headers, data = {
+            'orderId' : orderId,
+            'amount' : amount,
+            'currency' : currency,
+            'dateTime': dateTime,
+            'language': language,
+            'depositorUserId': userId,
+            'depositorTier': '0',
+            'depositMethod':  mymethod,
+            'depositorEmail': CustomUser.objects.filter(email=email),
+            'depositorName': CustomUser.objects.filter(first_name=first_name),
+            'redirectUrl': 'https://www.google.com',
+            'messageAuthenticationCode': my_hmac,
+        })
+        
+        rdata = r.json()
+        #print(rdata)
+        user = CustomUser.objects.get(username=rdata['depositTransaction']['depositorUserId'])
+        if r.status_code == 201:   
+            for x in Transaction._meta.get_field('currency').choices:
+                if rdata["depositTransaction"]["currency"] == x[1]:
+                    cur_val = x[0]
+
+            for y in Transaction._meta.get_field('status').choices:
+                if rdata["depositTransaction"]["status"] ==y[1]:
+                    cur_status = y[0] 
+            create = Transaction.objects.get_or_create(
+                order_id= rdata['orderId'],
+                #transaction_id=rdata["depositTransaction"]["transactionId"],
+                request_time=rdata["depositTransaction"]["dateCreated"],
+                amount=rdata["depositTransaction"]["amount"],
+                status=cur_status,
+                user_id=user,
+                method= rdata["depositTransaction"]["depositMethod"],
+                currency= cur_val,
+                
+            )
+        else:
+            logger.error('post information is not correct, please try again')
+        
+        return Response(rdata)
+
        
