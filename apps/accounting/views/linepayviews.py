3efa86649fc08d69ff5ddf407369b873a330a72d
Orion Ou <orion@claymoreusa.com>
2019-06-14 09:27:46 -0700

    remove print statement

--- apps/accounting/views/linepayviews.py
+++ apps/accounting/views/linepayviews.py
@@ -1,29 +1,27 @@
 from django.http import HttpResponse, JsonResponse, Http404, HttpResponseRedirect
-from djauth.third_party_keys import CHANNEL_ID, CHANNEL_SECRET
+from djauth.third_party_keys import LINE_CHANNEL_ID, LINE_CHANNEL_SECRET
 
 import requests
 
 LINE_PAYMENTS_SANDBOX_URL = "https://sandbox-api-pay.line.me/v2/payments/"
 
 def reservePayment(request):
-    print(request)
-    # if(request.method) # can only allow post requests
-
-    # prepare request to LINE pay server
-    requestURL = LINE_PAYMENTS_SANDBOX_URL + "request"
-    headers = {
-        "X-LINE-ChannelId": CHANNEL_ID,
-        "X-LINE-ChannelSecret": CHANNEL_SECRET
-    }
-    # orderId = #generate orderId to be unique 
-    payload = {
-       	"productName": "iBet-Orion-Test",
-        "productImageUrl": "https://ddowiki.com/images/Menace_of_the_Underdark_adpack_icon.jpg",
-        "amount": 123.12,
-        "currency": "THB",
-        "confirmUrl": "www.ibet.com/line-transfer-confirm",
-        "orderId": "1a2b3c" # need to generate orderId and store into database
-    }
-    
-    response = requests.post(requestURL, json=payload, headers=headers)
-    return JsonResponse(response.json())
+    if(request.method == "POST"): # can only allow post requests
+        # prepare request to LINE pay server
+        requestURL = LINE_PAYMENTS_SANDBOX_URL + "request"
+        headers = {
+            "X-LINE-ChannelId": LINE_CHANNEL_ID,
+            "X-LINE-ChannelSecret": LINE_CHANNEL_SECRET
+        }
+        # orderId = #generate orderId to be unique 
+        payload = {
+            "productName": "iBet-Orion-Test",
+            "productImageUrl": "https://ddowiki.com/images/Menace_of_the_Underdark_adpack_icon.jpg",
+            "amount": 123.12,
+            "currency": "THB",
+            "confirmUrl": "www.ibet.com/line-transfer-confirm",
+            "orderId": "1a2b3c" 
+        }
+        
+        response = requests.post(requestURL, json=payload, headers=headers)
+        return JsonResponse(response.json())
