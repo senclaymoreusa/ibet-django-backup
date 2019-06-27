ac7b1b02400c00c6547bfd4084c4c9ba1d781f85
VickyClaymore <vicky@claymoreusa.com>
2019-06-26 23:13:24 -0700

    permission related model

---
+++ apps/systemManagement/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+# permission code data will be hardcode in the codebase.
+
+# user group entity model
+class UserGroup(models.Model):
+
+    description = models.CharField(_('Description'), max_length=200, blank=True, null=True)
+    groupType = models.SmallIntegerField(_('Group Type'), blank=True, null=True, choices=GROUP_TYPE)
+    created_time = models.DateTimeField(
+        _('Created Time'),
+        auto_now_add=True,
+        editable=False,
+    )
+
+# many to many relation for permission and user group
+class PermissionGroup(models.Model):
+    
+    group = models.ForeignKey(UserGroup, on_delete=models.CASCADE, verbose_name=_('User Group'))
+    permission_code = models.CharField(_('Permission Code'), max_length=50)
+
+
+# many to many relation for user and user group
+class UserToUserGroup(models.Model):
+
+    group = models.ForeignKey(UserGroup, on_delete=models.CASCADE, verbose_name=_('User Group'))
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_('Custom User'))
+
+
+# many to many relation for user and standalone permission
+class UserPermission(models.Model):
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_('Custom User'))
+    permission_code = models.CharField(_('Permission Code'), max_length=50)
+    
