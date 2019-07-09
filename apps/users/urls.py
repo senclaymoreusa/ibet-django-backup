5d16850003c8a92e26068ee239b7c78e19554919
Stephen983 <yikang@claymoreusa.com>
2019-07-09 12:05:44 -0700

    Added wallet settle path

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -53,5 +53,6 @@ urlpatterns += [
     path('api/cancelregistration/', views.CancelRegistration.as_view(), name='cancel_registration'),
     path('api/getusernamebyreferid/', views.GetUsernameByReferid.as_view(), name = 'get_user'),
     path('api/walletgeneral/', views.WalletGeneralAPI.as_view(), name='wallet_general'),
-    path('api/walletbet/', views.WalletBetAPIURL.as_view(), name='wallet_bet')
+    path('api/walletbet/', views.WalletBetAPIURL.as_view(), name='wallet_bet'),
+    path('api/walletsettle', views.WalletSettleAPIURL.as_view(), name='wallet_settle')
 ]
