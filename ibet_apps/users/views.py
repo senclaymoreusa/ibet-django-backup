dc9625f4d046e174722435381e36c15265d0ef20
Stephen983 <yikang@claymoreusa.com>
2019-07-22 10:22:20 -0700

    balance

--- ibet_apps/users/views.py
+++ ibet_apps/users/views.py
@@ -3557,7 +3557,6 @@ class PostTransferforAG(APIView):
                 gametype        = dic['Data']['Record']['gametype']
                 gameCode        = dic['Data']['Record']['gameCode']
                 tableCode       = dic['Data']['Record']['tableCode']
-                transactionType = dic['Data']['Record']['transactionType']
                 transactionCode = dic['Data']['Record']['transactionCode']
                 playtype        = dic['Data']['Record']['playtype']
 
@@ -3599,6 +3598,107 @@ class PostTransferforAG(APIView):
                     playtype = playtype
                 )
 
+            except:
+
+                Status = status.HTTP_400_BAD_REQUEST
+                ResponseCode = 'INVALID_DATA'
+
+        elif transactionType == 'BALANCE':
+
+            try:
+
+                sessionToken     = dic['Data']['Record']['sessionToken']
+                playname         = dic['Data']['Record']['playname']
+
+                i = 0
+                while playname[i].isalpha():
+                    i += 1
+                while playname[i].isnumeric():
+                    i += 1
+            
+                username = playname[i:]
+
+                try:
+
+                    user = CustomUser.objects.get(username = username)
+                    balance = user.main_wallet
+                    Status = status.HTTP_200_OK
+                    ResponseCode = 'OK'
+
+                except:
+
+                    Status = status.HTTP_400_BAD_REQUEST
+                    ResponseCode = 'INVALID_DATA'
+
+                AGGamemodels.objects.create(
+                    sessionToken = sessionToken,
+                    playname     = playname,
+                    transactionType = transactionType
+                )
+
+            except:
+
+                Status = status.HTTP_400_BAD_REQUEST
+                ResponseCode = 'INVALID_DATA'
+
+        elif transactionType == 'WITHDRAW':
+
+            try:
+
+                sessionToken     = dic['Data']['Record']['sessionToken']
+                playname         = dic['Data']['Record']['playname']
+                transactionID    = dic['Data']['Record']['transactionID']
+                currency         = dic['Data']['Record']['currency']
+                amount           = dic['Data']['Record']['amount']
+                gameId           = dic['Data']['Record']['gameId']
+                roundId          = dic['Data']['Record']['roundId']
+                time             = dic['Data']['Record']['time']
+                remark           = dic['Data']['Record']['remark']
+
+                #print(sessionToken, playname, transactionType, transactionID, currency, amount, gameId, roundId, time, remark)
+
+                i = 0
+                while playname[i].isalpha():
+                    i += 1
+                while playname[i].isnumeric():
+                    i += 1
+            
+                username = playname[i:]
+
+                try:
+
+                    user = CustomUser.objects.filter(username = username)
+                    balance = user[0].main_wallet
+
+                    if balance >= decimal.Decimal(amount):
+                        balance -= decimal.Decimal(amount)
+                        user.update(main_wallet=balance, modified_time=timezone.now())
+                        ResponseCode = 'OK'
+                        Status = status.HTTP_200_OK
+
+                    else:
+                        
+                        Status = status.HTTP_409_CONFLICT
+                        ResponseCode = 'INSUFFICIENT_FUNDS'
+
+                except:
+                    Status = status.HTTP_400_BAD_REQUEST
+                    ResponseCode = 'INVALID_DATA'
+
+                AGGamemodels.objects.create(
+                    sessionToken     = sessionToken, 
+                    playname         = playname, 
+                    transactionType  = transactionType, 
+                    transactionID    = transactionID, 
+                    currency         = currency, 
+                    amount           = amount, 
+                    gameId           = gameId, 
+                    roundId          = roundId, 
+                    time             = time, 
+                    remark           = remark,
+                )
+
+            
             except:
 
                 Status = status.HTTP_400_BAD_REQUEST
