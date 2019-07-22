c7c71824b7da6023624059e0fdea623a0de7545c
Stephen983 <yikang@claymoreusa.com>
2019-07-22 10:14:15 -0700

    draw

--- ibet_apps/users/views.py
+++ ibet_apps/users/views.py
@@ -3415,6 +3415,132 @@ class PostTransferforAG(APIView):
                 Status = status.HTTP_400_BAD_REQUEST
                 ResponseCode = 'INVALID_DATA'
 
+        elif transactionType == 'LOSE':
+
+            try:
+
+                sessionToken    = dic['Data']['Record']['sessionToken']
+                currency        = dic['Data']['Record']['currency']
+                netAmount       = dic['Data']['Record']['netAmount']
+                validBetAmount  = dic['Data']['Record']['validBetAmount']
+                playname        = dic['Data']['Record']['playname']
+                agentCode       = dic['Data']['Record']['agentCode']
+                settletime      = dic['Data']['Record']['settletime']
+                transactionID   = dic['Data']['Record']['transactionID']
+                billNo          = dic['Data']['Record']['billNo']
+                gametype        = dic['Data']['Record']['gametype']
+                gameCode        = dic['Data']['Record']['gameCode']
+                transactionCode = dic['Data']['Record']['transactionCode']
+                ticketStatus    = dic['Data']['Record']['ticketStatus']
+                gameResult      = dic['Data']['Record']['gameResult']
+                finish          = dic['Data']['Record']['finish']
+
+                #print(sessionToken, currency, netAmount, validBetAmount, playname, agentCode, settletime, transactionID, billNo, gametype, gameCode, transactionType, transactionCode, ticketStatus, gameResult, finish)
+
+                username = playname[len(agentCode):]
+
+                try:
+
+                    user = CustomUser.objects.filter(username = username)
+                    balance = user[0].main_wallet
+                    balance += decimal.Decimal(validBetAmount) + decimal.Decimal(netAmount)
+                    user.update(main_wallet=balance, modified_time=timezone.now())
+                    ResponseCode = 'OK'
+                    Status = status.HTTP_200_OK
+
+                except:
+
+                    Status = status.HTTP_400_BAD_REQUEST
+                    ResponseCode = 'INVALID_DATA'
+
+                AGGamemodels.objects.create(
+                    sessionToken    = sessionToken, 
+                    currency        = currency, 
+                    netAmount       = netAmount, 
+                    validBetAmount  = validBetAmount, 
+                    playname        = playname, 
+                    agentCode       = agentCode, 
+                    settletime      = settletime, 
+                    transactionID   = transactionID, 
+                    billNo          = billNo, 
+                    gametype        = gametype, 
+                    gameCode        = gameCode, 
+                    transactionType = transactionType, 
+                    transactionCode = transactionCode, 
+                    ticketStatus    = ticketStatus, 
+                    gameResult      = gameResult, 
+                    finish          = finish
+                )
+
+            except:
+
+                Status = status.HTTP_400_BAD_REQUEST
+                ResponseCode = 'INVALID_DATA'
+
+        elif transactionType == 'WIN' and True: # This shares the same parameter with Win, needs to be further implemented
+
+            try:
+
+                sessionToken    = dic['Data']['Record']['sessionToken']
+                currency        = dic['Data']['Record']['currency']
+                netAmount       = dic['Data']['Record']['netAmount']
+                validBetAmount  = dic['Data']['Record']['validBetAmount']
+                playname        = dic['Data']['Record']['playname']
+                agentCode       = dic['Data']['Record']['agentCode']
+                settletime      = dic['Data']['Record']['settletime']
+                transactionID   = dic['Data']['Record']['transactionID']
+                billNo          = dic['Data']['Record']['billNo']
+                gametype        = dic['Data']['Record']['gametype']
+                gameCode        = dic['Data']['Record']['gameCode']
+                transactionCode = dic['Data']['Record']['transactionCode']
+                ticketStatus    = dic['Data']['Record']['ticketStatus']
+                gameResult      = dic['Data']['Record']['gameResult']
+                finish          = dic['Data']['Record']['finish']
+
+                #print(sessionToken, currency, netAmount, validBetAmount, playname, agentCode, settletime, transactionID, billNo, gametype, gameCode, transactionType, transactionCode, ticketStatus, gameResult, finish)
+
+                username = playname[len(agentCode):]
+
+                try:
+
+                    user = CustomUser.objects.filter(username = username)
+                    balance = user[0].main_wallet
+                    balance += decimal.Decimal(validBetAmount) + decimal.Decimal(netAmount)
+                    user.update(main_wallet=balance, modified_time=timezone.now())
+                    ResponseCode = 'OK'
+                    Status = status.HTTP_200_OK
+
+                except:
+
+                    Status = status.HTTP_400_BAD_REQUEST
+                    ResponseCode = 'INVALID_DATA'
+
+
+                AGGamemodels.objects.create(
+                    sessionToken     = sessionToken, 
+                    currency         = currency, 
+                    netAmount        = netAmount, 
+                    validBetAmount   =  validBetAmount, 
+                    playname         =  playname, 
+                    agentCode        =  agentCode, 
+                    settletime       = settletime, 
+                    transactionID    = transactionID, 
+                    billNo           =  billNo, 
+                    gametype         =  gametype, 
+                    gameCode         =  gameCode, 
+                    transactionType  =  transactionType, 
+                    transactionCode  =  transactionCode, 
+                    ticketStatus     =  ticketStatus, 
+                    gameResult       =   gameResult, 
+                    finish  =  finish
+                )
+
+            except:
+
+                Status = status.HTTP_400_BAD_REQUEST
+                ResponseCode = 'INVALID_DATA'
+
+
         response_data = '''<?xml version=”1.0” encoding=”UTF-8” standalone=”yes”?><TransferResponse><ResponseCode>{}</ResponseCode><Balance>{}</Balance></TransferResponse>'''.format(ResponseCode, balance)
 
         return Response(response_data, status=Status)
