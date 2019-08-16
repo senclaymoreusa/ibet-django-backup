8195eb4320f59d94d48c9db0caa71fff475225d9
wluuuu0801 <lu@claymoreusa.com>
2019-08-16 13:40:20 -0700

    change quotation mark back

--- ibet_apps/utils/constants.py
+++ ibet_apps/utils/constants.py
@@ -1,68 +1,77 @@
-GENDER_CHOICES = (("Male", "Male"), ("Female", "Female"))
+GENDER_CHOICES = (
+    ('Male','Male'),
+    ('Female', 'Female')
+)
 
 CONTACT_OPTIONS = (
-    ("Email", "Email"),
-    ("SMS", "SMS"),
-    ("OMS", "OMS"),
-    ("Push_Notification", "Push Notification"),
+    ('Email', 'Email'),
+    ('SMS', 'SMS'),
+    ('OMS', 'OMS'),
+    ('Push_Notification', 'Push Notification')
 )
 
 CURRENCY_TYPES = (
-    ("USD", "USD"),
-    ("EUR", "EUR"),
-    ("JPY", "JPY"),
-    ("CNY", "CNY"),
-    ("HKD", "HKD"),
-    ("AUD", "AUD"),
-    ("THB", "THB"),
-    ("MYR", "MYR"),
-    ("VND", "VND"),
-    ("MMK", "MMK"),
-    ("XBT", "XBT"),
+    ('USD', 'USD'),
+    ('EUR', 'EUR'),
+    ('JPY', 'JPY'),
+    ('CNY', 'CNY'),
+    ('HKD', 'HKD'),
+    ('AUD', 'AUD'),
+    ('THB','THB'),
+    ('MYR', 'MYR'),
+    ('VND', 'VND'),
+    ('MMK', 'MMK'),
+    ('XBT', 'XBT')
+
 )
 
-USERNAME_REGEX = "^[a-zA-Z0-9.+-]*$"
+USERNAME_REGEX = '^[a-zA-Z0-9.+-]*$'
 
 CHANNEL_CHOICES = (
-    (0, "Help2Pay"),
-    (1, "LINEpay"),
-    (2, "AstroPay"),
-    (3, "Qaicash"),
-    (4, "AsiaPay"),
-    (5, "Paypal"),
-    (6, "Payzod"),
-    (7, "CirclePay"),
-    (8, "Fgate"),
+    (0, 'Help2Pay'),
+    (1, 'LINEpay'),
+    (2, 'AstroPay'),
+    (3, 'Qaicash'),
+    (4, 'AsiaPay'),
+    (5, 'Paypal'),
+    (6, 'Payzod'),
+    (7, 'CirclePay'),
+    (8, 'Fgate')
 )
 
 currency_cny = 0
 
 CURRENCY_CHOICES = (
-    (0, "CNY"),
-    (1, "USD"),
-    (2, "THB"),
-    (3, "IDR"),
-    (4, "HKD"),
-    (5, "AUD"),
-    (6, "THB"),
-    (7, "MYR"),
-    (8, "VND"),
-    (9, "MMK"),
-    (10, "XBT"),
+    (0, 'CNY'),
+    (1, 'USD'),
+    (2, 'THB'),
+    (3, 'IDR'),
+    (4, 'HKD'),
+    (5, 'AUD'),
+    (6, 'THB'),
+    (7, 'MYR'),
+    (8, 'VND'),
+    (9, 'MMK'),
+    (10, 'XBT')
 )
 STATE_CHOICES = (
-    (0, "SUCCESS"),
-    (1, "FAILED"),
-    (2, "CREATED"),
-    (3, "PENDING"),
-    (4, "APPROVED"),
-    (5, "CANCELED"),
-    (6, "COMPLETED"),
-    (7, "RESEND"),
-    (8, "REJECTED"),
-    (9, "HELD"),
+    (0, 'SUCCESS'), 
+    (1, 'FAILED'),
+    (2, 'CREATED'),
+    (3, 'PENDING'),
+    (4, 'APPROVED'),
+    (5, 'CANCELED'),
+    (6, 'COMPLETED'),
+    (7, 'RESEND'),
+    (8, 'REJECTED'),
+    (9, 'HELD'),
+
 )
-REVIEW_STATE_CHOICES = ((0, "Approved"), (1, "Pending"), (2, "Rejected"))
+REVIEW_STATE_CHOICES = (
+    (0, 'Approved'),
+    (1, 'Pending'),
+    (2, 'Rejected'),
+)
 
 DEPOSIT_METHOD_CHOICES = (
     (0, "LBT_ONLINE"),
@@ -70,65 +79,76 @@ DEPOSIT_METHOD_CHOICES = (
     (2, "LBT_OTC"),
     (3, "DIRECT_PAYMENT"),
     (4, "BANK_TRANSFER"),
-    (5, "IBT"),
+    (5, "IBT")
 )
 
 transaction_deposit = 0
 transaction_withdrawl = 1
 
 TRANSACTION_TYPE_CHOICES = (
-    (0, "Deposit"),
-    (1, "Withdrawal"),
-    (2, "Bet Placed"),
-    (3, "Bet Settled"),
-    (4, "Transfer In"),
-    (5, "Transfer Out"),
-    (6, "Bonus"),
-    (7, "Adjustment"),
-    (8, "Commission"),
+    (0, 'Deposit'),
+    (1, 'Withdrawal'),
+    (2, 'Bet Placed'),
+    (3, 'Bet Settled'),
+    (4, 'Transfer In'),
+    (5, 'Transfer Out'),
+    (6, 'Bonus'),
+    (7, 'Adjustment'),
+    (8, 'Commission')
 )
 LANGUAGE_CHOICES = (
-    ("en-Us", "English – United States"),
-    ("zh-Hans", "Chinese Simplified"),
-    ("th", "Thai"),
-    ("vi", "Vietnamese"),
-    ("ko", "Korean"),
-    ("ja", "Japanese"),
+    ('en-Us', 'English – United States'),
+    ('zh-Hans', 'Chinese Simplified'),
+    ('th', 'Thai'),
+    ('vi', 'Vietnamese'),
+    ('ko', 'Korean'),
+    ('ja', 'Japanese'),
 )
 GAME_TYPE_CHOICES = (
-    (0, "Sports"),
-    (1, "Games"),
-    (2, "Live Casino"),
-    (3, "Financial"),
-    (4, "General"),
+    (0, 'Sports'),
+    (1, 'Games'),
+    (2, 'Live Casino'),
+    (3, 'Financial'),
+    (4, 'General'),
 )
 
 THIRDPARTY_STATUS_CHOICES = ((0, "OPEN"), (1, "CLOSE"))
 
 COUNTRY_CHOICES = (
-    ('US', "United States"),
-    ("CN", "China"),
-    ("TH", "Thailand"),
-    ("JP", "Japan"),
+    ('US', 'United States'),
+    ('CN', 'China'),
+    ('TH', 'Thailand'),
+    ('JP', 'Japan'),
 )
 
-ACTIVITY_TYPE = ((0, "Operation"), (1, "Remark"), (2, "Chat"), (3, "Note"))
+ACTIVITY_TYPE = (
+    (0, 'Operation'),
+    (1, 'Remark'),
+    (2, 'Chat'),
+    (3, 'Note'),
+)
 
 AGENT_LEVEL = (
-    ("Premium", "Premium"),
-    ("Invalid", "Invalid"),
-    ("Normal", "Normal"),
-    ("Negative", "Negative"),
+    ('Premium', 'Premium'),
+    ('Invalid', 'Invalid'),
+    ('Normal', 'Normal'),
+    ('Negative', 'Negative'),
 )
 
-AGENT_STATUS = (("Normal", "Normal"), ("Block", "Block"))
+AGENT_STATUS = (
+    ('Normal', 'Normal'),
+    ('Block', 'Block'),
+)
 
 PERMISSION_GROUP = 0
 OTHER_GROUP = 1
 
-GROUP_TYPE = ((PERMISSION_GROUP, "Permission"), (OTHER_GROUP, "other"))
+GROUP_TYPE = (
+    (PERMISSION_GROUP, 'Permission'),
+    (OTHER_GROUP, 'other')
+)
 
-
+   
 BANK_LIST_CHOICES = (
     ("OOO6CN", "China UnionPay"),
     ("ABOCCN", "Agricultural Bank of China"),
@@ -146,36 +166,36 @@ BANK_LIST_CHOICES = (
     ("CN01CN", "Zhongshan Rural Credit Union"),
 )
 
-LIMIT_TYPE_BET = 0
-LIMIT_TYPE_LOSS = 1
-LIMIT_TYPE_DEPOSIT = 2
-LIMIT_TYPE_WITHDRAW = 3
+LIMIT_TYPE_BET         = 0
+LIMIT_TYPE_LOSS        = 1
+LIMIT_TYPE_DEPOSIT     = 2
+LIMIT_TYPE_WITHDRAW    = 3
 LIMIT_TYPE_ACCESS_DENY = 4
 LIMIT_TYPE_BLOCK = 5
 LIMIT_TYPE_UNBLOCK = 6
 
 LIMIT_TYPE = (
-    (LIMIT_TYPE_BET, "Bet"),
-    (LIMIT_TYPE_LOSS, "Loss"),
-    (LIMIT_TYPE_DEPOSIT, "Deposit"),
-    (LIMIT_TYPE_WITHDRAW, "Withdraw"),
-    (LIMIT_TYPE_ACCESS_DENY, "Access Deny"),
-    (LIMIT_TYPE_BLOCK, "Block"),
-    (LIMIT_TYPE_UNBLOCK, "Unblock"),
+    (LIMIT_TYPE_BET, 'Bet'),
+    (LIMIT_TYPE_LOSS, 'Loss'),
+    (LIMIT_TYPE_DEPOSIT, 'Deposit'),
+    (LIMIT_TYPE_WITHDRAW, 'Withdraw'),
+    (LIMIT_TYPE_ACCESS_DENY, 'Access Deny'),
+    (LIMIT_TYPE_BLOCK, 'Block'),
+    (LIMIT_TYPE_UNBLOCK, 'Unblock')
 )
 
-GAME_PRODUCT_GB_SPORTS = 0
-GAME_PRODUCT_EZ_SPORTS = 1
-GAME_PRODUCT_GAMES = 2
-GAME_PRODUCT_LOTTO = 3
-GAME_PRODUCT_LIVE_CASINO = 4
+GAME_PRODUCT_GB_SPORTS         = 0
+GAME_PRODUCT_EZ_SPORTS         = 1
+GAME_PRODUCT_GAMES             = 2
+GAME_PRODUCT_LOTTO             = 3
+GAME_PRODUCT_LIVE_CASINO       = 4
 
 GAME_PRODUCT = (
-    (GAME_PRODUCT_GB_SPORTS, "GB SPORTS"),
-    (GAME_PRODUCT_EZ_SPORTS, "EZ SPORTS"),
-    (GAME_PRODUCT_GAMES, "GAMES"),
-    (GAME_PRODUCT_LOTTO, "LOTTO"),
-    (GAME_PRODUCT_LIVE_CASINO, "LIVE CASINO"),
+    (GAME_PRODUCT_GB_SPORTS, 'GB SPORTS'),
+    (GAME_PRODUCT_EZ_SPORTS, 'EZ SPORTS'),
+    (GAME_PRODUCT_GAMES, 'GAMES'),
+    (GAME_PRODUCT_LOTTO, 'LOTTO'),
+    (GAME_PRODUCT_LIVE_CASINO, 'LIVE CASINO')
 )
 
 
@@ -185,10 +205,10 @@ RISK_LEVEL_E2 = 2
 RISK_LEVEL_F = 3
 
 RISK_LEVEL = (
-    (RISK_LEVEL_A, "A"),
-    (RISK_LEVEL_E1, "E1"),
-    (RISK_LEVEL_E2, "E2"),
-    (RISK_LEVEL_F, "F"),
+    (RISK_LEVEL_A, 'A'),
+    (RISK_LEVEL_E1, 'E1'),
+    (RISK_LEVEL_E2, 'E2'),
+    (RISK_LEVEL_F, 'F'),
 )
 
 INTERVAL_PER_DAY = 0
@@ -200,46 +220,47 @@ INTERVAL_PER_THREE_YEAR = 5
 INTERVAL_PER_FIVE_YEAR = 6
 
 INTERVAL = (
-    (INTERVAL_PER_DAY, "per day"),
-    (INTERVAL_PER_WEEK, "per week"),
-    (INTERVAL_PER_MONTH, "per month"),
-    (INTERVAL_PER_SIX_MONTH, "per six months"),
-    (INTERVAL_PER_ONE_YEAR, "per one year"),
-    (INTERVAL_PER_THREE_YEAR, "per three years"),
-    (INTERVAL_PER_FIVE_YEAR, "per five years"),
+    (INTERVAL_PER_DAY, 'per day'),
+    (INTERVAL_PER_WEEK, 'per week'),
+    (INTERVAL_PER_MONTH, 'per month'),
+    (INTERVAL_PER_SIX_MONTH, 'per six months'),
+    (INTERVAL_PER_ONE_YEAR, 'per one year'),
+    (INTERVAL_PER_THREE_YEAR, 'per three years'),
+    (INTERVAL_PER_FIVE_YEAR, 'per five years'),
 )
 
 
 ASIAPAY_BANK_CHOICES = (
-    ("1", "工商银行"),
-    ("2", "建设银行"),
-    ("3", "农业银行"),
-    ("4", "招商银行"),
-    ("6", "广发银行"),
-    ("7", "中国银行"),
-    ("9", "中国邮政储蓄银行"),
-    ("10", "中信银行"),
-    ("11", "光大银行"),
-    ("12", "民生银行"),
-    ("16", "兴业银行"),
-    ("17", "华夏银行"),
-    ("23", "平安银行"),
-    ("38", "微信支付"),
-    ("39", "快捷支付"),
-    ("41", "支付宝"),
-    ("47", "银联支付"),
-    ("49", "京东支付"),
-    ("201", "比特币"),
+    ('1','工商银行'),
+    ('2','建设银行'),
+    ('3','农业银行'),
+    ('4','招商银行'),
+    ('6','广发银行'),
+    ('7','中国银行'),
+    ('9','中国邮政储蓄银行'),
+    ('10','中信银行'),
+    ('11','光大银行'),
+    ('12','民生银行'),
+    ('16','兴业银行'),
+    ('17','华夏银行'),
+    ('23','平安银行'),
+    ('38','微信支付'),
+    ('39','快捷支付'),
+    ('41','支付宝'),
+    ('47','银联支付'),
+    ('49','京东支付'),
+    ('201', '比特币')
 )
 ASIAPAY_PAYWAY_CHOICES = (
-    ("90", "比特币"),
-    ("41", "仅支持固定存入金额"),
-    ("42", "QRCode"),
-    ("44", "收银台"),
-    ("30", "在线支付"),
-    ("31", "另开视窗"),
-    ("10", "工行网银转账"),
-    ("11", "工行手机支付"),
+    ('90','比特币'),
+    ('41', '仅支持固定存入金额'),
+    ('42', 'QRCode'),
+    ('44', '收银台'),
+    ('30', '在线支付'),
+    ('31','另开视窗'),
+    ('10', '工行网银转账'),
+    ('11', '工行手机支付'),
+
 )
 
 GAME_PROVIDER_NETENT = 0
@@ -260,37 +281,39 @@ GAME_PROVIDER_KALAMBA = 14
 GAME_PROVIDER_LEANDER = 15
 GAME_PROVIDER_LIGHTNING_BOX = 16
 GAME_PROVIDER_NEXTGON = 17
-GAME_PROVIDER_RED7 = 18
-GAME_PROVIDER_RED_TIGET_GAMING = 19
-GAME_PROVIDER_SCIENTIFIC_GAMES = 20
+GAME_PROVIDER_RED7= 18
+GAME_PROVIDER_RED_TIGET_GAMING= 19
+GAME_PROVIDER_SCIENTIFIC_GAMES= 20
 GAME_PROVIDER_THUNDERKICK = 21
 GAME_PROVIDER_YGGDRASIL = 22
 
 
+
+
 GAME_PROVIDERS = (
-    (GAME_PROVIDER_NETENT, "Netent"),
-    (GAME_PROVIDER_PLAY_GO, "Play'n Go"),
-    (GAME_PROVIDER_BIG_TIME_GAMING, "Big Time Gaming"),
-    (GAME_PROVIDER_MICROGAMING, "Microgaming"),
-    (GAME_PROVIDER_QUICKSPIN, "Quickspin"),
-    (GAME_PROVIDER_PRAGMATIC_PLAY, "Progmatic Play"),
-    (GAME_PROVIDER_BLUEPRINT, "Blueprint"),
-    (GAME_PROVIDER_NOVOMATIC, "Novomatic"),
-    (GAME_PROVIDER_IGT, "IGT"),
-    (GAME_PROVIDER_ELK_STUDIOS, "Elk Studio"),
-    (GAME_PROVIDER_GENESIS, "Genesis"),
-    (GAME_PROVIDER_HIGH5, "High5"),
-    (GAME_PROVIDER_IRON_DOG, "Iron Dog"),
-    (GAME_PROVIDER_JUST_FOR_THE_WIN, "Just For The Win"),
-    (GAME_PROVIDER_KALAMBA, "Kalamba"),
-    (GAME_PROVIDER_LEANDER, "Leander"),
-    (GAME_PROVIDER_LIGHTNING_BOX, "Lightning Box"),
-    (GAME_PROVIDER_NEXTGON, "Nextgen"),
-    (GAME_PROVIDER_RED7, "Red7"),
-    (GAME_PROVIDER_RED_TIGET_GAMING, "Red Tiger Gaming"),
-    (GAME_PROVIDER_SCIENTIFIC_GAMES, "Scientific Games"),
-    (GAME_PROVIDER_THUNDERKICK, "Thunderkick"),
-    (GAME_PROVIDER_YGGDRASIL, "Yggdrasil"),
+    (GAME_PROVIDER_NETENT, 'Netent'),
+    (GAME_PROVIDER_PLAY_GO, 'Play\'n Go'),
+    (GAME_PROVIDER_BIG_TIME_GAMING, 'Big Time Gaming'),
+    (GAME_PROVIDER_MICROGAMING, 'Microgaming'),
+    (GAME_PROVIDER_QUICKSPIN, 'Quickspin'),
+    (GAME_PROVIDER_PRAGMATIC_PLAY, 'Progmatic Play'),
+    (GAME_PROVIDER_BLUEPRINT, 'Blueprint'),
+    (GAME_PROVIDER_NOVOMATIC, 'Novomatic'),
+    (GAME_PROVIDER_IGT, 'IGT'),
+    (GAME_PROVIDER_ELK_STUDIOS, 'Elk Studio'),
+    (GAME_PROVIDER_GENESIS, 'Genesis'),
+    (GAME_PROVIDER_HIGH5, 'High5'),
+    (GAME_PROVIDER_IRON_DOG, 'Iron Dog'),
+    (GAME_PROVIDER_JUST_FOR_THE_WIN, 'Just For The Win'),
+    (GAME_PROVIDER_KALAMBA, 'Kalamba'),
+    (GAME_PROVIDER_LEANDER, 'Leander'),
+    (GAME_PROVIDER_LIGHTNING_BOX, 'Lightning Box'),
+    (GAME_PROVIDER_NEXTGON, 'Nextgen'),
+    (GAME_PROVIDER_RED7, 'Red7'),
+    (GAME_PROVIDER_RED_TIGET_GAMING, 'Red Tiger Gaming'),
+    (GAME_PROVIDER_SCIENTIFIC_GAMES, 'Scientific Games'),
+    (GAME_PROVIDER_THUNDERKICK, 'Thunderkick'),
+    (GAME_PROVIDER_YGGDRASIL, 'Yggdrasil'),
 )
 
 CATEGORY_TYPES_SPORTS = 0
@@ -299,11 +322,12 @@ CATEGORY_TYPES_SLOTS = 2
 CATEGORY_TYPES_LOTTERY = 3
 
 
+
 CATEGORY_TYPES = (
-    (CATEGORY_TYPES_SPORTS, "SPORTS"),
-    (CATEGORY_TYPES_LIVE_CASINO, "LIVE CASINO"),
-    (CATEGORY_TYPES_SLOTS, "SLOTS"),
-    (CATEGORY_TYPES_LOTTERY, "LOTTERY"),
+    (CATEGORY_TYPES_SPORTS, 'SPORTS'),
+    (CATEGORY_TYPES_LIVE_CASINO, 'LIVE CASINO'),
+    (CATEGORY_TYPES_SLOTS, 'SLOTS'),
+    (CATEGORY_TYPES_LOTTERY, 'LOTTERY'),
 )
 
 GAME_ATTRIBUTES_GAME_CATEGORY = 0
@@ -313,56 +337,56 @@ GAME_ATTRIBUTES_FEATURES = 3
 GAME_ATTRIBUTES_THEME = 4
 
 GAME_ATTRIBUTES = (
-    (GAME_ATTRIBUTES_GAME_CATEGORY, "Games Category"),
-    (GAME_ATTRIBUTES_JACKPOT, "Jackpot"),
-    (GAME_ATTRIBUTES_PROVIDER, "Provider"),
-    (GAME_ATTRIBUTES_FEATURES, "Features"),
-    (GAME_ATTRIBUTES_THEME, "Theme"),
+    (GAME_ATTRIBUTES_GAME_CATEGORY, 'Games Category'),
+    (GAME_ATTRIBUTES_JACKPOT, 'Jackpot'),
+    (GAME_ATTRIBUTES_PROVIDER, 'Provider'),
+    (GAME_ATTRIBUTES_FEATURES, 'Features'),
+    (GAME_ATTRIBUTES_THEME, 'Theme'),
 )
 
 
-ASIAPAY_CMDTYPE = (("01", "查询存款订单"), ("02", "查询提款订单"))
-ASIAPAY_CASHOUTMETHOD_CHOICES = (("CashSCBatch", "加密货币"), ("cashifacebatch", "代付"))
+ASIAPAY_CMDTYPE = (
+    ('01', '查询存款订单'),
+    ('02', '查询提款订单'),
+)
+ASIAPAY_CASHOUTMETHOD_CHOICES = (
+    ('CashSCBatch', '加密货币'),
+    ('cashifacebatch', '代付'),
+)
 
 LOCALHOST = "http://localhost:3000"
 DEV_URL = "https://ibet-web-dev.claymoreusa.net"
 
 # LINEpay
 LINE_PAYMENTS_SANDBOX_URL = "https://sandbox-api-pay.line.me/v2/payments/"
-PRODUCT_IMG_URL = (
-    "https://pathtoproductimage.jpg"
-)  # dummy image, will be replaced with actual company URL later
+PRODUCT_IMG_URL = "https://pathtoproductimage.jpg"  # dummy image, will be replaced with actual company URL later
 
 # qaicash-payment
-QAICASH_URL = "https://public-services.mekong-300.com/ago/integration/"
-MERCHANTID = "39"
-MERCHANTAPIKEY = "70PsPAH!Z7l18ZuVo8^c"
-APIVERSION = "v2.0"
-DEPOSIT_URL = "/deposit/routing/"
-PAYOUT_URL = "/payout/routing/"
+QAICASH_URL = 'https://public-services.mekong-300.com/ago/integration/'
+MERCHANTID = '39'
+MERCHANTAPIKEY = '70PsPAH!Z7l18ZuVo8^c'
+APIVERSION = 'v2.0'
+DEPOSIT_URL = '/deposit/routing/'
+PAYOUT_URL = '/payout/routing/'
 
 # paypal-payment
-PAYPAL_MODE = "sandbox"  # sandbox or live
-PAYPAL_CLIENT_ID = (
-    "AXoM7FKTdT8rfh-SI66SlAWd_P85YSsNfTvm0zjB0-AhJhUhUHTuXi4L87DcgkxLSLPYKCMO5DVl2pDD"
-)
-PAYPAL_CLIENT_SECRET = (
-    "ENKmcu7Sci-RHW2gHvzmeUbZvSaCuwRiEirKH0_TkYo4AZWbVnfevS-hxq6cS6sevLU5TB3SMfq85wSB"
-)
-PAYPAL_SANDBOX_URL = "https://api.sandbox.paypal.com/"
+PAYPAL_MODE = 'sandbox'   # sandbox or live
+PAYPAL_CLIENT_ID = 'AXoM7FKTdT8rfh-SI66SlAWd_P85YSsNfTvm0zjB0-AhJhUhUHTuXi4L87DcgkxLSLPYKCMO5DVl2pDD'
+PAYPAL_CLIENT_SECRET = 'ENKmcu7Sci-RHW2gHvzmeUbZvSaCuwRiEirKH0_TkYo4AZWbVnfevS-hxq6cS6sevLU5TB3SMfq85wSB'
+PAYPAL_SANDBOX_URL = 'https://api.sandbox.paypal.com/'
 
 # astroPay sandbox url
-ASTROPAY_URL = "https://sandbox-api.astropaycard.com/"
+ASTROPAY_URL = 'https://sandbox-api.astropaycard.com/'
 
 
 # astroPay sandbox key:
-ASTROPAY_X_LOGIN = "1PboDQ2FySeUK8YmaJTkfVlFzy0zTMvQ"
-ASTROPAY_X_TRANS_KEY = "sQaDolJOA4cvlPoBwLXQjDAEnOO1XCjX"
+ASTROPAY_X_LOGIN = '1PboDQ2FySeUK8YmaJTkfVlFzy0zTMvQ' 
+ASTROPAY_X_TRANS_KEY = 'sQaDolJOA4cvlPoBwLXQjDAEnOO1XCjX'
 ASTROPAY_SECRET = "RJLuSCDcd6mj7SoinVzkH7g2ueJRlScH"
 
 # astroPay sandbod WEBPAYSTATUS:
-ASTROPAY_WP_LOGIN = "f1b1d639c5"
-ASTROPAY_WP_TRANS_KEY = "738e34417a"
+ASTROPAY_WP_LOGIN = 'f1b1d639c5'
+ASTROPAY_WP_TRANS_KEY = '738e34417a'
 
 # asia-pay
 ASIAPAY_API_URL = "http://gw.wave-pay.com"
@@ -411,101 +435,53 @@ FGATE_TYPE = "fgo"
 
 GAME_FILTER_OPTION = [
     {
-        "name": "Games Category",
-        "data": ["New", "Popular", "Table Games", "Slots", "All Games"],
+        'name': 'Games Category',
+        'data': ['New', 'Popular', 'Table Games', 'Slots', 'All Games']
     },
     {
-        "name": "Jackpot",
-        "data": [
-            "Daily Jackpots",
-            "Fixed Jackpots",
-            "Progressive Jackpot",
-            "Multiple Jackpots",
-        ],
+        'name': 'Jackpot',
+        'data': ['Daily Jackpots', 'Fixed Jackpots', 'Progressive Jackpot', 'Multiple Jackpots']
     },
     {
-        "name": "Provider",
-        "data": [
-            "Netent",
-            "Play'n Go",
-            "Big Time Gaming",
-            "Microgaming",
-            "Quickspin",
-            "Pragmatic Play",
-            "Blueprint",
-            "Novomatic",
-            "IGT",
-            "Elk Studios",
-            "Genesis",
-            "High5",
-            "Iron Dog",
-            "Just For The Win",
-            "Kalamba",
-            "Leander",
-            "Lightning Box",
-            "Nextgen",
-            "Red7",
-            "Red Tiger Gaming",
-            "Scientific Games",
-            "Thunderkick",
-            "Yggdrasil",
-            "Other",
-        ],
+        'name': 'Provider',
+        'data': ['Netent', 'Play\'n Go', 'Big Time Gaming', 'Microgaming', 'Quickspin', 'Pragmatic Play', 'Blueprint', 'Novomatic', 'IGT', 'Elk Studios',
+        'Genesis', 'High5', 'Iron Dog', 'Just For The Win', 'Kalamba', 'Leander', 'Lightning Box', 'Nextgen', 'Red7', 'Red Tiger Gaming', 'Scientific Games', 
+        'Thunderkick', 'Yggdrasil', 'Other']
     },
     {
-        "name": "Feature",
-        "data": [
-            "Megaways",
-            "Pay Both Ways",
-            "Bonus Feature",
-            "Free Spins",
-            "Double Or Nothing Feature",
-        ],
+        'name': 'Feature',
+        'data': ['Megaways', 'Pay Both Ways', 'Bonus Feature', 'Free Spins', 'Double Or Nothing Feature']
     },
     {
-        "name": "Theme",
-        "data": [
-            "Egypt",
-            "Oriental",
-            "Mythology",
-            "Animal",
-            "Adventure",
-            "Fruit",
-            "Western",
-            "Film / Tv",
-            "Music",
-            "Sports",
-            "Space",
-            "Holidays",
-            "Dark/ Halloween",
-            "Vegas",
-        ],
+        'name': 'Theme',
+        'data': ['Egypt', 'Oriental', 'Mythology', 'Animal', 'Adventure', 'Fruit', 'Western', 'Film / Tv', 'Music', 'Sports',
+            'Space', 'Holidays', 'Dark/ Halloween', 'Vegas']
     },
     {
-        "name": "Sort by",
-        "data": ["Name", "Popularity", "Jackpot Size Asc", "Jackpot Size Desc"],
+        'name': 'Sort by',
+        'data': ['Name', 'Popularity', 'Jackpot Size Asc', 'Jackpot Size Desc']
     },
 ]
 # Notification
-MESSAGE_ALERT = 1
+MESSAGE_ALERT  = 1
 MESSAGE_DIRECT = 2
 
 NOTIFICATION_TYPE = (
-    (MESSAGE_ALERT, "ALERT"),
-    (MESSAGE_DIRECT, "DIRECT"),
+    (MESSAGE_ALERT, 'ALERT'),
+    (MESSAGE_DIRECT, 'DIRECT'),
     # (3, 'REFERRAL')
 )
 
-NOTIFICATION_DIRECT = "D"
-NOTIFICATION_PUSH = "P"
-NOTIFICATION_SMS = "S"
-NOTIFICATION_EMAIL = "E"
+NOTIFICATION_DIRECT = 'D'
+NOTIFICATION_PUSH   = 'P'
+NOTIFICATION_SMS    = 'S'
+NOTIFICATION_EMAIL  = 'E' 
 
 NOTIFICATION_METHOD = (
-    (NOTIFICATION_DIRECT, "direct"),
-    (NOTIFICATION_PUSH, "push"),
-    (NOTIFICATION_SMS, "sms"),
-    (NOTIFICATION_EMAIL, "email"),
+    (NOTIFICATION_DIRECT, 'direct'),
+    (NOTIFICATION_PUSH, 'push'),
+    (NOTIFICATION_SMS, 'sms'),
+    (NOTIFICATION_EMAIL, 'email')
 )
 
 BONUS_QUEUE_NAME = "bonus_queue"
