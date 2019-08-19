5c15446e6f39eb65ff0da99918559c8475872921
Orion Ou <orion@claymoreusa.com>
2019-08-19 14:37:28 -0700

    fix error not finding client ip address

--- ibet_apps/accounting/views/asiapayviews.py
+++ ibet_apps/accounting/views/asiapayviews.py
@@ -47,13 +47,21 @@ bankidConversion = {
     '201': '比特币',
 }
 
-def get_Host_name_IP(): 
-    try: 
-        host_name = socket.gethostname() 
-        host_ip = socket.gethostbyname(host_name) 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_Host_name_IP():
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
         return host_ip
-    except: 
-        logger.info("Unable to get Hostname and IP") 
+    except:
+        logger.info("Unable to get Hostname and IP")
 
 def MD5(code):
     res = hashlib.md5(code.encode()).hexdigest()
@@ -93,7 +101,7 @@ class submitDeposit(generics.GenericAPIView):
        
         userid = self.request.POST.get("userid")
         uID = "n" + userid
-        UserIP= get_Host_name_IP()
+        UserIP= get_client_ip(request)
         TraceID = strftime("%Y%m%d%H%M%S", gmtime())
         OrderID =  "ibet" +strftime("%Y%m%d%H%M%S", gmtime())
         NoticeUrl = ""
