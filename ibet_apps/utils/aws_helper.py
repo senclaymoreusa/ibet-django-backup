4f9e932691bceb80a000f0aa16d02d5963958602
Orion Ou <orion@claymoreusa.com>
2019-08-21 15:45:39 -0700

    add region to sqs queue

--- ibet_apps/utils/aws_helper.py
+++ ibet_apps/utils/aws_helper.py
@@ -37,8 +37,9 @@ def getAWSClient(service_name, third_party_keys):
 
     return client 
 
+
 def getSQSQueue(third_party_keys, queue_name):
-    sqs = boto3.resource('sqs')
+    sqs = boto3.resource('sqs', region_name='eu-west-2')
     try:
         queue = sqs.get_queue_by_name(QueueName=queue_name)
     except ClientError as e:
