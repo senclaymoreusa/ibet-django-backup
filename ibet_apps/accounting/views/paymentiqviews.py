97cdbbae4025b2cef295fa18428c82568c3e965e
Orion Ou <orion@claymoreusa.com>
2019-09-11 10:52:33 -0700

    makemigration + code cleanup

--- ibet_apps/accounting/views/paymentiqviews.py
+++ ibet_apps/accounting/views/paymentiqviews.py
@@ -153,7 +153,7 @@ def authorize(request):
                     amount=float(post_data["txAmount"]),
                     method=post_data["provider"] + ": " + post_data["txName"],
                     currency=cyConversion[post_data["txAmountCy"]],
-                    transaction_type=1,  # 0 = DEPOSIT
+                    transaction_type=1,  # 1 = WITHDRAW
                     channel=10,  # 10 = PaymentIQ
                     status=2,  # 2 = CREATED
                     last_updated=timezone.now()
@@ -222,6 +222,7 @@ def transfer(request):
                         "errMsg": "Transfer failed: duplicate Transfer action"  # message explaining error
                     })
                 success = helpers.addOrWithdrawBalance(matching_transaction.user_id, matching_transaction.amount, "add")
+
                 matching_transaction.status = 0
                 matching_transaction.arrive_time = timezone.now()
                 matching_transaction.last_updated = timezone.now()
@@ -234,29 +235,50 @@ def transfer(request):
                     "txId": post_data["txId"],
                     "merchantTxId": matching_transaction.transaction_id,
                 }
-                print(result)
                 return JsonResponse(result)
 
             elif "withdraw" in post_data["txName"].lower():
-                Transaction.objects.create(
-                    user_id=post_data["userId"],
-                    transaction_id=trans_id,
+                matching_transaction = Transaction.objects.get(
+                    user_id=user,
+                    order_id=post_data["txId"],
                     amount=float(post_data["txAmount"]),
                     method=post_data["provider"] + ": " + post_data["txName"],
                     currency=cyConversion[post_data["txAmountCy"]],
-                    transaction_type=1,  # 1 = WITHDRAW
+                    transaction_type=0,  # 0 = DEPOSIT
                     channel=10,  # 10 = PaymentIQ
-                    status=2,  # 2 = CREATED
-                    last_updated=timezone.now()
                 )
-                return JsonResponse({
-                    "userId": post_data["userId"],
-                    "success": False,
-                    "txId": post_data["txId"],
-                    "merchantTxId": trans_id,
-                    "errCode": "001",
-                    "errMsg": "Authorize failed: not enough withdraw balance"
-                })
+                if matching_transaction.status != 2:
+                    return JsonResponse({
+                        "success": False,
+                        "errCode": "200",  # custom error code (used internally by ibet)
+                        "errMsg": "Transfer failed: duplicate Transfer action"  # message explaining error
+                    })
+
+                matching_transaction.status = 0
+                matching_transaction.arrive_time = timezone.now()
+                matching_transaction.last_updated = timezone.now()
+                matching_transaction.remark = "Successfully withdrew!"
+                matching_transaction.save()
+
+                success = helpers.addOrWithdrawBalance(matching_transaction.user_id, matching_transaction.amount, "withdraw")
+                if success:
+                    result = {
+                        "userId": post_data["userId"],
+                        "success": success,
+                        "txId": post_data["txId"],
+                        "merchantTxId": trans_id,
+                    }
+                    return JsonResponse(result)
+                else:
+                    result = {
+                        "userId": post_data["userId"],
+                        "success": success,
+                        "txId": post_data["txId"],
+                        "merchantTxId": trans_id,
+                        "errCode": "001",
+                        "errMsg": "Authorize failed: not enough withdraw balance"
+                    }
+                    return JsonResponse(result)
         except ObjectDoesNotExist as e:
             logger.info(e)
             return JsonResponse({
@@ -266,7 +288,6 @@ def transfer(request):
                 "errCode": "001",  # custom error code (used internally by ibet)
                 "errMsg": "Transfer failed: Transaction does not exist"  # message explaining error
             })
-        return JsonResponse({"msg": "hi"})
 
 
 def cancel(request):
