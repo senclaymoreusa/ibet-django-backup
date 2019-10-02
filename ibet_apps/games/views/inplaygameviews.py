2f8f72920b165e0d72c60b6219b0d0ba48e8bbb6
rundong-usaclaymore <rundong@claymoreusa.com>
2019-10-01 17:54:03 -0700

    log in api

--- ibet_apps/games/views/inplaygameviews.py
+++ ibet_apps/games/views/inplaygameviews.py
@@ -25,11 +25,94 @@ logger = logging.getLogger('django')
 
 
 class InplayLoginAPI(View):
-    return HttpResponse(status=200)
+    def get(self, request, *arg, **kwargs):
+        data = request.body
+        # print(data)
+        dic = xmltodict.parse(data)
+        # print(dic)
+
+        action  = dic['request']['@action']
+        requestId = dic['request']['element']['@id']
+        for i in dic['request']['element']['properties']:
+            if i['@name'] == 'userid':
+                propertiesUserId = i['#text']
+            else:
+                propertiesPassword = i['#text']
+
+        # print(action, requestId, propertiesUserId, propertiesPassword)
+
+        statusCode = 0
+        currencyCode = ""
+        errorMessage = "Successfully login"
+        try: 
+            user = CustomUser.objects.get(username=propertiesUserId)
+            if user.check_password(propertiesPassword) is False:
+                statusCode = 101
+                errorMessage = "Invalid password"
+
+            if user.block is True:
+                statusCode = 104
+                errorMessage = "User has been block"
+
+            if user.currency == "CNY":
+                currencyCode = "156"
+            elif user.currency == "THB":
+                currencyCode = "764"
+            elif user.currency == "VND":
+                currencyCode = "704"
+
+        except:
+            statusCode = 101
+            errorMessage = "Invalid user ID"
+
+        response = {
+            "request": {
+                "@action": "clogin",
+                "element": {
+                    "@id": requestId,
+                     "properties": [
+                        {
+                            "@name": "userid",
+                            "#text": propertiesUserId
+                        },
+                        {
+                            "@name": "username",
+                            "#text": propertiesUserId
+                        },
+                        {
+                            "@name": "acode",
+                            "#text": "null"
+                        },
+                        {
+                            "@name": "vendorid",
+                            "#text": "null" # will provide by EA
+                        },
+                        {
+                            "@name": "currencyid",
+                            "#text": str(currencyCode)
+                        }, 
+                        {
+                            "@name": "status",
+                            "#text": str(statusCode)
+
+                        },
+                        {
+                            "@name": "errdesc",
+                            "#text": str(errorMessage)
+                            
+                        }
+                    ]
+                }
+            } 
+        }
+        response = xmltodict.unparse(response, pretty=True)
+        return HttpResponse(response, content_type='text/xml')
 
 
 class InplayGetBalanceAPI(View):
-    return HttpResponse(status=200)
+    def get(self, request, *arg, **kwargs):
+        params = requests
+        return HttpResponse(status=200)
 
 
 class InplayGetApprovalAPI(View):
