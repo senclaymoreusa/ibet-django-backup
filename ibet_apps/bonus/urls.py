cee7157e0efcc878f9a08b264959eb3be2e30fec
ibetadmin <cai@ibet.com>
2019-10-09 22:31:31 -0700

    adding api for individual bonuses and some minor updates.

--- ibet_apps/bonus/urls.py
+++ ibet_apps/bonus/urls.py
@@ -8,5 +8,6 @@ from bonus.views import *
 urlpatterns = [
 
     path('api/bonus/', BonusSearchView.as_view(), name='bonus_search'),
+    path('api/bonus/<str:pk>', BonusView.as_view(), name="bonus_id"),
 
 ]
--- ibet_apps/bonus/views.py
+++ ibet_apps/bonus/views.py
@@ -34,7 +34,7 @@ class BonusSearchView(View):
             bonus_data = serializers.serialize('json', [req.bonus])
             bonus_data = json.loads(bonus_data)
             req_data = serializers.serialize('json', [req])
-            req_data = json.loads(req_data)
+            req_data = json.loads(req_data)[0]
 
             pk = bonus_data[0]['pk']
             if pk in bonuses:
@@ -44,7 +44,7 @@ class BonusSearchView(View):
                 bonuses[pk]['requirements'] = [req_data]
 
         # In rare cases, there could be bonuses that do not have any requirements
-        bonus_all = Bonus.objects.filter()
+        bonus_all = Bonus.objects.all()
         for bonus in bonus_all:
             if str(bonus.pk) in bonuses.keys():
                 continue
@@ -54,13 +54,32 @@ class BonusSearchView(View):
 
         return HttpResponse(json.dumps(bonuses), content_type='application/json')
 
+class BonusView(View):
 
+    # Returns a Bonus object in JSON (containing all the requirements as a list)
+    def get(self, request, *arg, **kwargs):
 
+        bonus_pk = self.kwargs.get('pk')
+        bonus = Bonus.objects.get(pk=bonus_pk)
 
+        bonus_data = serializers.serialize('json', {bonus})
+        bonus_data = json.loads(bonus_data)[0]['fields']
 
+        # We need to iterate through all the requirements as we do not have the mapping,
+        # but that should be fine performance-wise, as we don't have too many bonuses or requirements anyway.
+        reqs = Requirement.objects.all()
+        for req in reqs:
 
+            if bonus_pk != str(req.bonus.pk):
+                continue
 
+            req_data = serializers.serialize('json', [req])
+            req_data = json.loads(req_data)[0]
+            if 'requirements' in bonus_data.keys():
+                bonus_data['requirements'].append(req_data)
+            else:
+                bonus_data['requirements'] = [req_data]
 
-
+        return HttpResponse(json.dumps(bonus_data), content_type='application/json', status=200)
 
 
