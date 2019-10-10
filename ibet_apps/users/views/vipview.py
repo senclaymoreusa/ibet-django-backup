a2ac2d4c84ad96f0311f490d7019287cb1e48073
wluuuu0801 <lu@claymoreusa.com>
2019-10-10 16:48:45 -0700

    clean import

--- ibet_apps/users/views/vipview.py
+++ ibet_apps/users/views/vipview.py
@@ -1,33 +1,8 @@
-from django.contrib.auth import get_user_model
-from django.core import serializers
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse, reverse_lazy
-from django.views import generic
+from django.http import HttpResponse
 from django.views import View
-from django.utils.translation import ugettext_lazy as _
-from django.utils.timezone import timedelta, localtime, now
-from django.db.models import Count, Sum, Q
-from django.db.models.functions import TruncMonth, TruncYear, TruncDate, Coalesce
-from django.contrib import messages
+from django.utils import timezone
 from django.shortcuts import render
-from django.template.defaulttags import register
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.serializers.json import DjangoJSONEncoder
-from django.core.mail import EmailMultiAlternatives
-from django.dispatch import receiver
-from django.template.loader import render_to_string
-from django.conf import settings
-from dateutil.relativedelta import relativedelta
-from django.shortcuts import render_to_response
-from decimal import Decimal
-
 from xadmin.views import CommAdminView
-from utils.constants import *
-from accounting.models import *
-from users.models import CustomUser, UserAction, Commission, UserActivity, ReferLink
-from operation.models import Notification, NotificationToUsers
-from utils.admin_helper import *
-
 
 import logging
 import simplejson as json
