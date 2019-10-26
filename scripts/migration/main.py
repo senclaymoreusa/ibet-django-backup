9a02e6d4cb2abd1d4d258c95a9b35c9085e25760
ibetadmin <cai@ibet.com>
2019-10-25 22:13:36 -0700

    clean up the migration script.

--- scripts/migration/main.py
+++ scripts/migration/main.py
@@ -6,25 +6,16 @@
 ######################################################################################
 
 import pyodbc
-
 import os
 import boto3
 import json
-import logging
 import datetime
-import sys
 import psycopg2
-
-
 from botocore.exceptions import ClientError, NoCredentialsError
 from dotenv import load_dotenv
 from django.utils.translation import ugettext_lazy as _
 
 
-
-
-
-"""
 # There's really no point for logging, so I will use print everywhere.
 print ('Starting the Migration...')
 
@@ -36,18 +27,16 @@ username = 'migration'
 password = 'Clay1688'
 
 try:
+    print ('First, connecting to our SQL Server DB...')
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
 except Exception as e:
     print ("Error connecting to SQL Server DB, Exiting...", e)
     exit(-1)
 
-print ('Successfully connected to the DB:', server, database)
-"""
+print ('Successfully connected to the SQL Server:', server, database)
 
-print ('connecting to our postgres DB...')
-
-logger = logging.getLogger('django')
+print ('Second, connecting to our postgres DB...')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print("[" + str(datetime.datetime.now()) + "] Trying to load environment variables...")
@@ -57,13 +46,11 @@ if os.path.exists("/tmp/ibetenv/.env") or os.path.exists(BASE_DIR+"/.env"):
 else:
     print("[" + str(datetime.datetime.now()) + "] No .env file was found")
 
-
 load_dotenv()
 if "ENV" in os.environ:
     print("[" + str(datetime.datetime.now()) + "] Environment is: " + os.getenv("ENV"))
 else:
     print("[" + str(datetime.datetime.now()) + "] Environment not specified!")
-
 
 def getKeys(bucket, file):
     s3 = boto3.client('s3')
@@ -79,34 +66,12 @@ def getKeys(bucket, file):
 
     return config
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7i^ke1w79@h(!g0)e18c8(^j=c8ewfx8=*9n4652o%0f3i^g_-'
 
 print("[" + str(datetime.datetime.now()) + "] Using db of dev")
 AWS_S3_ADMIN_BUCKET = "ibet-admin-apdev"
 db_data = getKeys(AWS_S3_ADMIN_BUCKET, 'config/ibetadmin_db.json')
-
-
-print (db_data['RDS_DB_NAME'])
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': db_data['RDS_DB_NAME'],
-        'USER': db_data['RDS_USERNAME'],
-        'PASSWORD': db_data['RDS_PASSWORD'],
-        'HOST': db_data['RDS_HOSTNAME'],
-        'PORT': db_data['RDS_PORT'],
-    }
-}
 
 connection = psycopg2.connect(  user = db_data['RDS_USERNAME'],
                                 password = db_data['RDS_PASSWORD'],
@@ -116,33 +81,7 @@ connection = psycopg2.connect(  user = db_data['RDS_USERNAME'],
 
 cursor = connection.cursor()
 
-
-print ('connection successful')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print ('Successfully connected to our postgres DB.')
 
 
 
