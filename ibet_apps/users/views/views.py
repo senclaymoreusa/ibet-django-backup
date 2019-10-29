16a092a4fd33d63563534b148d862c0596e6f135
rundong-usaclaymore <rundong@claymoreusa.com>
2019-10-29 16:00:42 -0700

    fix verification bug

--- ibet_apps/users/views/views.py
+++ ibet_apps/users/views/views.py
@@ -1161,17 +1161,17 @@ class VerifyActivationCode(APIView):
         code = request.data['code']
         user = get_user_model().objects.filter(username=username)
         
-        last_verify_action = UserAction.objects.filter(Q(user=user)&Q(event_type=EVENT_CHOICES_SMS_CODE)).order_by('-created_time')[0]
+        # last_verify_action = UserAction.objects.filter(Q(user=user[0])&Q(event_type=EVENT_CHOICES_SMS_CODE)).order_by('-created_time')
 
-        delta = timezone.now() - timedelta(seconds=180)
-        if verification_time < last_verify_action.created_time:
-            if user[0].activation_code == code:
-                user.update(active=True)
-                user.update(activation_code='')
-                return Response(status=status.HTTP_200_OK)
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        else:
-            return Response(ERROR_CODE_TIME_EXCEED)
+        # delta = timezone.now() - timedelta(seconds=180)
+        # if verification_time < last_verify_action[0].created_time:
+        if user[0].activation_code == code:
+            user.update(active=True)
+            user.update(activation_code='')
+            return Response(status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+        # else:
+        #     return Response(ERROR_CODE_TIME_EXCEED)
 
 
 class UserSearchAutocomplete(View):
