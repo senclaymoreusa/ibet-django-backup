4a3d5e3473c9cb6879718bb7a016805bbd0398da
Orion Ou <orion@claymoreusa.com>
2019-11-13 15:27:24 -0800

    checkpoint for API implementations

--- ibet_apps/games/views/bti_views.py
+++ ibet_apps/games/views/bti_views.py
@@ -14,6 +14,7 @@ from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.conf import settings
 from django.utils import timezone
 from rest_framework.authtoken.models import Token
+from lxml import etree
 
 from users.views.helper import checkUserBlock
 from users.models import CustomUser
@@ -58,29 +59,67 @@ class Reserve(View):
         username = request.GET.get("cust_id")
         bet_id = request.GET.get("reserve_id")
         amount = decimal.Decimal(request.GET.get("amount"))
+        # txn_id = generateTxnId()
+        txn_id = "12345asdf"
         try:
             with transaction.atomic():
                 user = CustomUser.objects.get(username=username)
-                user.main_wallet = user.main_wallet - amount
+                if amount < user.main_wallet:
+                    new_balance = user.main_wallet - amount
+                    user.main_wallet = new_balance
 
-                bet = GameBet(
-                    user=user,
-                    ref_no=bet_id,
-                    amount_wagered=amount
-                )
-                bet.save()
-                user.save()
-
-        except (ObjectDoesNotExist, DatabaseError, IntegrityError) as e:
-            return
+                    bet = GameBet(
+                        user=user,
+                        ref_no=bet_id,
+                        amount_wagered=amount,
+                        # transaction_id=generateUID,
+                        # provider=PROVIDER,
+                        # category=CATEGORY,
+                    )
+                    bet.save()
+                    user.save()
+                else:
+                    raise Exception
+            res = "error_code=0\r\n"
+            res += "error_message=success\r\n"
+            res += f"balance={new_balance}\r\n"
+            res += f"trx_id={txn_id}\r\n"
+            res += f"BonusUsed=0.00\rn"
+            return HttpResponse(res, content_type='text/plain')
+        except (ObjectDoesNotExist, DatabaseError, IntegrityError, Exception) as e:
+            res = "error_code=-4\r\n"
+            res += "error_message=InsufficientFunds\r\n"
+            res += f"balance={new_balance}\r\n"
+            res += f"trx_id={txn_id}\r\n"
+            res += f"BonusUsed=0.00\r\n"
+            return HttpResponse(res, content_type='text/plain')
                 
-        return HttpResponse("Post Reserve")
 
 class DebitReserve(View):
-    def get(self, request):
-        return HttpResponse("Test test test")
     def post(self, request):
-        return HttpResponse("Post Debit Reserve")
+        username = request.GET.get("cust_id")
+        bet_id = request.GET.get("reserve_id")
+        amount = decimal.Decimal(request.GET.get("amount"))
+        req_id = request.GET.get("debit_reserve")
+        xmlData = etree.fromstring(request.body)
+        
+        txn_id = "12345asdf" #TODO: change this to be variable
+        try:
+            with transaction.atomic():
+                user = CustomUser.objects.get(username=username)
+
+            res = "error_code=0\r\n"
+            res += "error_message=success\r\n"
+            res += f"trx_id={txn_id}\r\n"
+            res += f"balance={user.main_wallet}\r\n" #TODO: remove hard coded balance
+
+            return HttpResponse(res, content_type='text/plain')
+        except (ObjectDoesNotExist, DatabaseError, IntegrityError, Exception) as e:
+            res = "error_code=-4\r\n"
+            res += "error_message=InsufficientFunds\r\n"
+            res += f"trx_id={txn_id}\r\n"
+            res += f"balance={user.main_wallet}\r\n"
+            return HttpResponse(res, content_type='text/plain')
 
 class CommitReserve(View):
     def get(self, request):
@@ -90,6 +129,45 @@ class CommitReserve(View):
 
 class CancelReserve(View):
     def get(self, request):
+        username = request.GET.get("cust_id")
+        bet_id = request.GET.get("reserve_id")
+        try:
+            with transaction.atomic():
+                user = CustomUser.objects.get(username=username)
+
+                try:
+                    prev_bet = GameBet.objects.get(ref_no=bet_id)
+                except ObjectDoesNotExist as e:
+                    bet = GameBet(
+                        ref_no=bet_id,
+                        amount_won=decimal.Decimal(0),
+                        outcome=3
+                        # provider=PROVIDER,
+                        # category=CATEGORY,
+                    )
+                    
+                bet = GameBet(
+                    ref_no=bet_id,
+                    amount_won=prev_bet.amount_wagered,
+                    # provider=PROVIDER,
+                    # category=CATEGORY,
+                )
+                new_balance = user.main_wallet + prev_bet.amount_wagered
+                user.main_wallet = new_balance
+                user.save()
+                bet.save()
+                
+            res = "error_code=-4\r\n"
+            res += "error_message=success\r\n"
+            # res += f"balance={user.main_wallet}\r\n"
+            res += f"balance=100\r\n"
+            return HttpResponse(res, content_type='text/plain')
+        except (ObjectDoesNotExist, DatabaseError, IntegrityError, Exception) as e:
+            logger.error(repr(e))
+            res = "error_code=-4\r\n"
+            res += "error_message=success\r\n"
+            # res += f"balance={user.main_wallet}\r\n"
+            res += f"balance=100\r\n"
+            return HttpResponse(res, content_type='text/plain')
+            
         return HttpResponse("Test test test")
-    def post(self, request):
-        return HttpResponse("Post validate Token")
