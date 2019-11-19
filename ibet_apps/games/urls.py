8c5873d85baf41018c59d2cfc30282b7dbff5673
Angela <angela@claymoreusa.com>
2019-11-18 16:12:45 -0800

    updated urls

--- ibet_apps/games/urls.py
+++ ibet_apps/games/urls.py
@@ -75,7 +75,7 @@ urlpatterns = [
     path('api/onebook/fund_transfer', onebookviews.FundTransfer.as_view(), name="fund_transfer"),
     path('api/onebook/login', onebookviews.Login.as_view(), name="Login"),
     path('api/onebook/check_member_online', csrf_exempt(onebookviews.CheckMemberOnline), name="Check_Member_Online"),
-    path('api/onebook/get_bet_detail', onebookviews.GetBetDetail.as_view(), name="Get_Bet_Detail"),
+    path('api/onebook/get_bet_detail', onebookviews.GetBetDetail, name="Get_Bet_Detail"),
     path('api/onebook/test', onebookviews.test.as_view(), name="onebook_test"),
     
 
