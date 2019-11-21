ba3a67b3c2211fd16e8d4e137bc3d6ffb1ffcda9
rundong-usaclaymore <rundong@claymoreusa.com>
2019-11-20 18:20:33 -0800

    deduct balance api

--- ibet_apps/games/views/inplaygameviews.py
+++ ibet_apps/games/views/inplaygameviews.py
@@ -6,6 +6,7 @@ from django.views import View
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.conf import settings
 from users.models import CustomUser
+from accounting.models import *
 import simplejson as json
 import xmltodict
 import decimal
@@ -167,14 +168,41 @@ class InplayDeductBalanceAPI(View):
             balance_package = "ZwgZhGFWmUv5vDi5q2ruVNNlKC+WU/nkctAdoxbVdOUeW+RbwyYE91w8OXAeAgw5G8cVCxZC5Lt6MFBoaBxSfTnRLW6RazhbRYyB4Fk76mo="
             print(balance_package)
             data = des3Decryption(balance_package)
+            data = "".join([data.rsplit("}" , 1)[0] , "}"]) 
             # print(data)
             # print(data["EventTypeId"])
             #  data = json.dumps(str(data))
-            data = json.loads(str(data))
-            if data["EventTypeId"] == 1003:
+            data = json.loads(data)
+            print(data)
+            if data["EventTypeId"] == '1003':
                 user = data["MemberCode"]
+                amount = float(data["TransactionAmt"])
                 user = CustomUser.objects.get(username=user)
-                amount = data["TransactionAmt"]
+                if user.main_wallet > amount:
+                    trans_id = user.username + "-" + timezone.datetime.today().isoformat() + "-" + str(random.randint(0, 10000000))
+                    trans = Transaction.objects.create(
+                        transaction_id=trans_id,
+                        user_id=user,
+                        order_id=trans_id,
+                        amount=amount,
+                        currency=user_currency,
+                        transfer_from="IMES",
+                        transfer_to="main",
+                        product=0,
+                        transaction_type=TRANSACTION_TRANSFER,
+                        status=TRAN_PENDING_TYPE
+                    )
+
+                    res = {}
+                    res["DateReceived"] = timezone.now()
+                    res["DateSent"] = timezone.now()
+                    res["StatusCode"] = 100
+                    res["StatusMessage"] = "Success"
+                    res["PackageId"] = 374
+                    res["Balance"] = 0.0
+
+                    return HttpResponse(json.dumps(res), content_type='application/json', status=200)
+                print(amount)
         except Exception as e:
             print("Error: {}".format(repr(e)))
 
@@ -207,7 +235,9 @@ class TestDecryption(View):
 
             print(cipher_json)
 
-            plain_json = des3Decryption(cipher_json).strip()
+            plain_json = des3Decryption(cipher_json)
+
+            plain_json = "".join([plain_json.rsplit("}" , 1)[0] , "}"]) 
             print(plain_json)
 
             plain_json = json.loads(plain_json)
