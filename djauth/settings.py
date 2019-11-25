58841f93d5c0c87a6d86d0171a78eaddf1920a68
Angela <angela@claymoreusa.com>
2019-11-25 09:54:16 -0800

    updated redis

--- djauth/settings.py
+++ djauth/settings.py
@@ -230,7 +230,7 @@ elif "ENV" in os.environ:
     print("[" + str(datetime.datetime.now()) + "] Using staging Redis...")
     REDIS = {
         # "HOST": 'staging-redis-cluster.hivulc.clustercfg.apne1.cache.amazonaws.com',
-        "HOST": 'letou-staging.hivulc.clustercfg.apne1.cache.amazonaws.com:6379',
+        "HOST": 'letou-staging.hivulc.clustercfg.apne1.cache.amazonaws.com',
         "PORT": 6379
     }
 
