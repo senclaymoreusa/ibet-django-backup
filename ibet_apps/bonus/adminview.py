d7751a54e37f2fbdec2e4362f4ed576f219c2ad1
wluuuu0801 <lu@claymoreusa.com>
2019-12-13 04:11:53 -0800

    add bonus transaction page

--- ibet_apps/bonus/adminview.py
+++ ibet_apps/bonus/adminview.py
@@ -25,3 +25,15 @@ class BonusRecordsView(CommAdminView):
         context['groups'] = UserGroup.objects.all()
         context['must_have'] = BONUS_MUST_HAVE
         return render(request, "bonus_records.html", context)
+
+
+class BonusTransactionsView(CommAdminView):
+
+    def get(self, request):
+        context = super().get_context()
+        context["breadcrumbs"].append("Bonuses / Bonus transactions")
+        context['time'] = timezone.now()
+        context['bonuses_types'] = BONUS_TYPE_CHOICES
+        context['bonuses_status'] = BONUS_STATUS_CHOICES
+        context['bonus_event'] = UserBonusEvent.objects.all()
+        return render(request, "bonus_transactions.html", context)
