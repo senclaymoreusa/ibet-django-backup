0cae310d4f32e9710ad2f885bc47404ec38f389a
Orion Ou <orion@claymoreusa.com>
2019-12-18 12:28:45 -0800

    getBetHistory view

--- ibet_apps/games/views/betsviews.py
+++ ibet_apps/games/views/betsviews.py
@@ -1,15 +1,50 @@
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.db.models import Q
 
 from games.models import GameBet
 from users.models import CustomUser
 
+from django.utils import timezone
+from datetime import datetime
+import pytz
+
 
 def getBetHistory(request):
     print(request)
+    # date range (start/end)
+    # provider
+    # category
+    # status (open/closed)
     if request.method == "GET":
-        user = CustomUser.objects.get(username=request.GET["userid"])
-        all_bets = GameBet.objects.select_related().filter(username=user).order_by('-bet_time')
+        username = request.GET.get("userid")
+        if not username:
+            return HttpResponse(status=404)
+
+        user = CustomUser.objects.get(username=username)
+
+        all_bets = GameBet.objects.select_related().filter(user=user).order_by('-bet_time')
+
+        if request.GET.get("status") and request.GET.get("status") == "open":
+            all_bets = all_bets.filter(resolved_time__isnull=True)
+        if request.GET.get("status") and request.GET.get("status") == "closed":
+            all_bets = all_bets.filter(resolved_time__isnull=False)
+
+        if request.GET.get("provider"):
+            provider = request.GET.get("provider")
+            all_bets = all_bets.filter(provider=provider)
+        
+        if request.GET.get("start"):
+            startStr = request.GET.get("start")
+            startDate = datetime.strptime(startStr, "%Y-%m-%d")
+
+            all_bets = all_bets.filter(bet_time__gte=pytz.utc.localize(startDate))
+
+        if request.GET.get("end"):
+            endStr = request.GET.get("end")
+            endDate = datetime.strptime(endStr, "%Y-%m-%d")
+            all_bets = all_bets.filter(bet_time__lte=pytz.utc.localize(endDate))
+
         bet_data = []
         for bet in all_bets:
             data = dict()
@@ -23,5 +58,5 @@ def getBetHistory(request):
 
         return JsonResponse({
             'results': bet_data,
-            'full_raw_data': list(all_bets.values)
+            'full_raw_data': list(all_bets.values())
         })
