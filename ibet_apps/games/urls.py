4ecb1d5ab36a364a137087f8118ef2b80f1c5093
rundong-usaclaymore <rundong@claymoreusa.com>
2019-12-18 13:38:00 -0800

    csrf

--- ibet_apps/games/urls.py
+++ ibet_apps/games/urls.py
@@ -82,7 +82,7 @@ urlpatterns = [
     path('api/ky/games/', csrf_exempt(kygameviews.KaiyuanAPI.as_view()), name="ky_games"),
     path('api/ky/test/', csrf_exempt(kygameviews.TestTransferAPI.as_view()), name="ky_test"),
     path('api/ky/record/', csrf_exempt(kygameviews.TestGetRecord.as_view()), name="ky_record"),
-    path('api/ky/getbets/', kygameviews.KyBets.as_view(), name='ky_bets'),
+    path('api/ky/getbets/', csrf_exempt(kygameviews.KyBets.as_view()), name='ky_bets'),
 
     #onebook
     path('api/onebook/create_member', onebookviews.CreateMember.as_view(), name="create_member"),
