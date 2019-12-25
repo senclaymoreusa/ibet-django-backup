df5094b6817120d3b82f3687333fb1127d0fb60c
VickyClaymore <vicky@claymoreusa.com>
2019-12-24 16:16:31 -0800

    fixed comment

--- ibet_apps/games/views/views.py
+++ ibet_apps/games/views/views.py
@@ -21,140 +21,131 @@ logger = logging.getLogger('django')
 class GamesSearchView(View):
 
     def get(self, request,  *args, **kwargs):
-        q = request.GET.get('q')
-        # gameType = request.GET.get('type')
-        category = request.GET.get('category')
-        filterCategory = request.GET.get('filtercategory')
-        jackpot = request.GET.get('jackpot')
-        provider = request.GET.get('provider')
-        feature = request.GET.get('feature')
-        theme = request.GET.get('theme')
-        sort = request.GET.get('sort')
 
-        # print("q: " + str(q))
-        # print("type: " + str(gameType))
-        # print("category: " + str(category))
-        # print("filterCategory: " + str(filterCategory))
-        # print("jackpot: " + str(jackpot))
-        # print("provider: " + str(provider))
-        # print("sort: " + str(sort))
-        # print('theme:' + str(theme))
-        # print('featrue:' + str(feature))
-
-        attributeList = []
-        providerList = []
-        # if gameType:
-        #     attributeList = attributeList + gameType.split()
-        # if category:
-        #     attributeList = attributeList + category.split()
-
-        if provider:
-            providerList = provider.split('+')
-        if filterCategory:
-            attributeList = attributeList + filterCategory.split('+')
-        if jackpot:
-            attributeList = attributeList + jackpot.split('+')
-        if feature:
-            attributeList = attributeList + feature.split('+')
-        if theme:
-            attributeList = attributeList + theme.split('+')
-        
-        # print(providerList)
-        # print(str(attributeList))
-        # print("feature: " + str(feature))
-        # print("theme: " + str(theme))
-        # print("sort: " + str(sort))
         try:
-            games_category = Category.objects.get(name='Games')
-            all_child_category_of_games = Category.objects.filter(parent_id=games_category)
-        except:
-            logger.error("Cannot find Games category")
+            q = request.GET.get('q')
+            # gameType = request.GET.get('type')
+            category = request.GET.get('category')
+            filterCategory = request.GET.get('filtercategory')
+            jackpot = request.GET.get('jackpot')
+            provider = request.GET.get('provider')
+            feature = request.GET.get('feature')
+            theme = request.GET.get('theme')
+            sort = request.GET.get('sort')
+
+            attributeList = []
+            providerList = []
+            # if gameType:
+            #     attributeList = attributeList + gameType.split()
+            # if category:
+            #     attributeList = attributeList + category.split()
+
+            if provider:
+                providerList = provider.split('+')
+            if filterCategory:
+                attributeList = attributeList + filterCategory.split('+')
+            if jackpot:
+                attributeList = attributeList + jackpot.split('+')
+            if feature:
+                attributeList = attributeList + feature.split('+')
+            if theme:
+                attributeList = attributeList + theme.split('+')
+            
+            try:
+                games_category = Category.objects.get(name='Games')
+                all_child_category_of_games = Category.objects.filter(parent_id=games_category)
+            except:
+                logger.error("Cannot find Games category")
         
-        category_pk_arr = []
-        for i in all_child_category_of_games:
-            category_pk_arr.append(
-                { 'pk': str(i.pk), 'name': i.name }
-            )
-        
-        gameFilter = (Q(category_id__parent_id=games_category)|Q(category_id=games_category))
-        if q:
-            gameFilter &= (
-                # Q(name__icontains=q)|Q(name_zh__icontains=q)|
-                # Q(name_fr__icontains=q)|Q(description__icontains=q)|
-                # Q(description_zh__icontains=q)|Q(description_fr__icontains=q)| 
-                # Q(attribute__icontains=q)|Q(category_id__name__icontains=q)
-                Q(name__icontains=q)|Q(name_zh__icontains=q)|
-                Q(name_fr__icontains=q)
-            )
-            logger.info("Searching key word: " + str(q) + "from all games")
-
-        # if gameType:
-        #     # print(str(gameType))
-        #     gameFilter = gameFilter & Q(category_id__parent_id__name__iexact=gameType)
-        #     if category != 'all' and category:
-        #         gameFilter = gameFilter & Q(category_id__name__iexact=category)
-        #     logger.info("Filter by game category: " + str(gameType))
-        if category != 'all' and category:
-            # category = category.split('-')
-            category = ' '.join(category.split('-'))
-            gameFilter = gameFilter & Q(category_id__name__iexact=category)
-
-        # providerFilter = Q()
-        # if provider:
-        #     gameFilter |= (
-        #         Q(provider__provider_name__icontains=q)
-            # all_providers = GameProvider.objects.all()
-            # for each_provider in providerList:
-            #     # print('provider: ' + str(provider))
-            #     for gameProvider in GAME_PROVIDERS:
-            #         name = gameProvider[1]
-            #         if provider.lower() == name.lower():
-            #             providerFilter = providerFilter | Q(provider=gameProvider[0])
-            #         else:
-            #             providerFilter = providerFilter | Q(provider=-1)
-            # GameProvider.objects.all()
-
-
-        # gameFilter = gameFilter & providerFilter
-        providerFilter = Q()
-        if provider:
-            for i in providerList:
-                providerFilter |= (
-                    Q(provider__provider_name__icontains=i)
+            category_pk_arr = []
+            for i in all_child_category_of_games:
+                category_pk_arr.append(
+                    { 'pk': str(i.pk), 'name': i.name }
                 )
-        gameFilter &= providerFilter
+            
+            gameFilter = (Q(category_id__parent_id=games_category)|Q(category_id=games_category))
+            if q:
+                gameFilter &= (
+                    # Q(name__icontains=q)|Q(name_zh__icontains=q)|
+                    # Q(name_fr__icontains=q)|Q(description__icontains=q)|
+                    # Q(description_zh__icontains=q)|Q(description_fr__icontains=q)| 
+                    # Q(attribute__icontains=q)|Q(category_id__name__icontains=q)
+                    Q(name__icontains=q)|Q(name_zh__icontains=q)|
+                    Q(name_fr__icontains=q)
+                )
+                logger.info("Searching key word: " + str(q) + "from all games")
 
-        for attr in attributeList:
-            gameFilter = gameFilter & Q(attribute__icontains=attr)
-            logger.info("Filter by attributes: " + str(attr)) 
+            # if gameType:
+            #     # print(str(gameType))
+            #     gameFilter = gameFilter & Q(category_id__parent_id__name__iexact=gameType)
+            #     if category != 'all' and category:
+            #         gameFilter = gameFilter & Q(category_id__name__iexact=category)
+            #     logger.info("Filter by game category: " + str(gameType))
+            if category != 'all' and category:
+                # category = category.split('-')
+                category = ' '.join(category.split('-'))
+                gameFilter = gameFilter & Q(category_id__name__iexact=category)
 
-        if sort == 'popularity':
-            data = Game.objects.filter(gameFilter).order_by('-popularity')
-            logger.info("Order list of games by: popularity") 
-        elif sort == 'jackpot size asc':
-            data = Game.objects.filter(gameFilter).order_by(F('jackpot_size').asc(nulls_last=True))
-            logger.info("Order list of games by: jackpot size asc") 
-        elif sort == 'jackpot size desc':
-            data = Game.objects.filter(gameFilter).order_by(F('jackpot_size').desc(nulls_last=True))
-            logger.info("Order list of games by: jackpot size desc") 
-        else:
-            data = Game.objects.filter(gameFilter).order_by('name')
-            logger.info("Re-order list of games alphabetically by name") 
-        
-        
-        if not data:
-            logger.info('Search q did not match any categories or token')
-        
-        data = serializers.serialize('json', data)
-        data = json.loads(data)
+            # providerFilter = Q()
+            # if provider:
+            #     gameFilter |= (
+            #         Q(provider__provider_name__icontains=q)
+                # all_providers = GameProvider.objects.all()
+                # for each_provider in providerList:
+                #     # print('provider: ' + str(provider))
+                #     for gameProvider in GAME_PROVIDERS:
+                #         name = gameProvider[1]
+                #         if provider.lower() == name.lower():
+                #             providerFilter = providerFilter | Q(provider=gameProvider[0])
+                #         else:
+                #             providerFilter = providerFilter | Q(provider=-1)
+                # GameProvider.objects.all()
 
-        for i in data:
-            for category_obj in category_pk_arr:
-                if category_obj['pk'] == i['fields']['category_id']:
-                    i['fields']['category_name'] = category_obj['name']
 
-        logger.info("Sending search game results response......... ")
-        return HttpResponse(json.dumps(data), content_type='application/json')
+            # gameFilter = gameFilter & providerFilter
+            providerFilter = Q()
+            if provider:
+                for i in providerList:
+                    providerFilter |= (
+                        Q(provider__provider_name__icontains=i)
+                    )
+            gameFilter &= providerFilter
+
+            for attr in attributeList:
+                gameFilter = gameFilter & Q(attribute__icontains=attr)
+                logger.info("Filter by attributes: " + str(attr)) 
+
+            if sort == 'popularity':
+                data = Game.objects.filter(gameFilter).order_by('-popularity')
+                logger.info("Order list of games by: popularity") 
+            elif sort == 'jackpot size asc':
+                data = Game.objects.filter(gameFilter).order_by(F('jackpot_size').asc(nulls_last=True))
+                logger.info("Order list of games by: jackpot size asc") 
+            elif sort == 'jackpot size desc':
+                data = Game.objects.filter(gameFilter).order_by(F('jackpot_size').desc(nulls_last=True))
+                logger.info("Order list of games by: jackpot size desc") 
+            else:
+                data = Game.objects.filter(gameFilter).order_by('name')
+                logger.info("Re-order list of games alphabetically by name") 
+            
+            
+            if not data:
+                logger.info('Search q did not match any categories or token')
+            
+            data = serializers.serialize('json', data)
+            data = json.loads(data)
+
+            for i in data:
+                for category_obj in category_pk_arr:
+                    if category_obj['pk'] == i['fields']['category_id']:
+                        i['fields']['category_name'] = category_obj['name']
+
+            logger.info("Sending search game results response......... ")
+            return HttpResponse(json.dumps(data), content_type='application/json')
+
+        except Exception as e:
+            logger.error("Getting game list:", e)
+            return HttpResponse(status=400)
 
 
 class ProvidersSearchView(View):
