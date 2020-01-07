d7569f0cd918eaf4148d1774ee5ad1d93a6333bd
Orion Ou <orion@claymoreusa.com>
2020-01-06 16:14:09 -0800

    modify bet history to return transaction ids

--- ibet_apps/games/views/betsviews.py
+++ ibet_apps/games/views/betsviews.py
@@ -4,22 +4,95 @@ from django.core import serializers
 from games.models import GameBet
 from users.models import CustomUser
 
+from django.utils import timezone
+from datetime import datetime
+import pytz
+import logging
+import collections
+
+logger = logging.getLogger("django")
+
+
+def getProvidersAndCategories(request):
+    if request.method == "GET":
+        p = GameProvider.objects.all()
+        c = Category.objects.filter(parent_id__isnull=True)
+
+        return JsonResponse({
+            "providers": list(p.values()),
+            "categories": list(c.values())
+        })
 
 def getBetHistory(request):
-    print(request)
-    if request.method == "GET":
-        user = CustomUser.objects.get(username=request.GET["userid"])
-        all_bets = GameBet.objects.select_related().filter(username=user).order_by('-bet_time')
-        bet_data = []
-        for bet in all_bets:
-            data = dict()
-            data["amount_wagered"] = bet.amount_wagered
-            data["amount_won"] = bet.amount_won
-            data["outcome"] = bet.get_outcome_display()
-            data["date"] = bet.bet_time
-            data["category"] = bet.category.name
-            data["provider"] = bet.provider.provider_name
-            bet_data.append(data)
+    # filter by: date range (start/end),
+    # provider,
+    # category,
+    # status (open/closed)
+
+    try:
+        if request.method == "GET":
+            user_id = request.GET.get("userid")
+            if not user_id:
+                return HttpResponse(status=404)
+
+            if not request.GET.get("start") and not request.GET.get("end"):
+                logger.info("Getting bet history: You have to select start or end date")
+                return JsonResponse({
+                    'success': False,
+                    'results': "You have to select start or end date"
+                })
+
+            user = CustomUser.objects.get(pk=user_id)
+
+            all_bets = GameBet.objects.select_related().filter(user=user).order_by('-bet_time')
+
+            # if request.GET.get("status") and request.GET.get("status") == "open":
+            #     all_bets = all_bets.filter(resolved_time__isnull=True)
+            # if request.GET.get("status") and request.GET.get("status") == "closed":
+            #     all_bets = all_bets.filter(resolved_time__isnull=False)
+
+            if request.GET.get("provider"):
+                provider = request.GET.get("provider")
+                all_bets = all_bets.filter(provider=GameProvider.objects.get(pk=provider))
+
+            if request.GET.get("category"):
+                category = request.GET.get("category")
+                all_bets = all_bets.filter(category_id=Category.objects.get(pk=category))
+            
+            if request.GET.get("start"):
+                startStr = request.GET.get("start")
+                startDate = datetime.strptime(startStr, "%Y/%m/%d")
+                all_bets = all_bets.filter(bet_time__gte=pytz.utc.localize(startDate))
+
+            if request.GET.get("end"):
+                endStr = request.GET.get("end")
+                endDate = datetime.strptime(endStr, "%Y/%m/%d")
+                all_bets = all_bets.filter(bet_time__lte=pytz.utc.localize(endDate))
+            
+            # all_bets.group_by = ['ref_no']
+
+            bet_data = collections.defaultdict(lambda: None)
+            
+            for bet in all_bets:
+                data = dict()
+                data["amount_wagered"] = bet.amount_wagered
+                data["amount_won"] = bet.amount_won
+                data["outcome"] = bet.get_outcome_display()
+                data["date"] = bet.bet_time
+                data["category"] = bet.category.name
+                data["provider"] = bet.provider.provider_name
+                data["ref_no"] = bet.ref_no
+                if bet_data[bet.ref_no]:
+                    bet_data[bet.ref_no].append(data)
+                else:
+                    bet_data[bet.ref_no] = [data]
+            
+            logger.info("Successfully get bet history")
+            return JsonResponse({
+                'success': True,
+                'results': bet_data,
+                'full_raw_data': list(all_bets.values())
+            })
 
         return JsonResponse({
             'results': bet_data,
