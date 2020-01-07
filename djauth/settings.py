c84c0f33e3c6267d54b8afcfd62ef12dabacbb24
sen <sen@claymoreusa.com>
2020-01-06 17:20:45 -0800

    added pathname to formatter

--- djauth/settings.py
+++ djauth/settings.py
@@ -374,7 +374,7 @@ else:
         'disable_existing_loggers': False,
         'formatters': {
             'verbose': {
-                'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                'format': "[%(asctime)s] %(levelname)s [%(pathname)s:%(lineno)s] %(message)s",
                 'datefmt': "%d/%b/%Y %H:%M:%S"
             },
             'simple': {
