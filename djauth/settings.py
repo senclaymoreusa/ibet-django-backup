90b6646d6f3fafb3df70b5f434789fdbe736099b
sen <sen@claymoreusa.com>
2020-01-06 17:09:56 -0800

    added log formatter

--- djauth/settings.py
+++ djauth/settings.py
@@ -372,11 +372,21 @@ else:
     LOGGING = {
         'version': 1,
         'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                'datefmt': "%d/%b/%Y %H:%M:%S"
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
+        }, 
         'handlers': {
             'file': {
                 'level': 'ERROR',
                 'class': 'logging.FileHandler',
                 'filename': '/opt/python/log/django-logger.log',
+                'formatter': 'verbose',
             }
         },
         'loggers': {
