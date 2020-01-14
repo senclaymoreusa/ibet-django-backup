d5c3e95703fa4560af404bca5c5801a054adca19
Orion Ou <orion@claymoreusa.com>
2020-01-13 16:59:32 -0800

    fix typo

--- ibet_apps/utils/helpers.py
+++ ibet_apps/utils/helpers.py
@@ -27,11 +27,11 @@ def addOrWithdrawBalance(username, balance, type_balance):
                 logger.info("User's current balance is: " + str(current_balance))
                 new_balance = current_balance + decimal.Decimal(balance)
                 user.main_wallet = new_balance
-                user.modified_time = timezone.now())
+                user.modified_time = timezone.now()
                 referrer = user.referred_by
 
                 logger.info("User's new balance is: " + str(new_balance))
-                
+
                 if referrer:
                     referr_object = get_user_model().objects.filter(username=referrer.username)
                     data = Config.objects.all()[0]
