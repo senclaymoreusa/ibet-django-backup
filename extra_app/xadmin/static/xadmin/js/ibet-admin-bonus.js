82df22b056b50ca8356c8b542ece7dce5d8b54d9
wluuuu0801 <lu@claymoreusa.com>
2020-01-21 17:47:22 -0800

    clean console

--- extra_app/xadmin/static/xadmin/js/ibet-admin-bonus.js
+++ extra_app/xadmin/static/xadmin/js/ibet-admin-bonus.js
@@ -451,7 +451,6 @@ $(document).ready(function() {
 
     $('#bonus-requirements').chosen({width: "242px"}).change(function(){
         wagerRequirements = $(this).chosen().val();
-        console.log(wagerRequirements)
     });
 
     $('#group-unassigned').chosen({width: "242px"}).change(function(){
