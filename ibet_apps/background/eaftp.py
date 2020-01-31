1b86d9915b3efa384e9eb06cebaa6dd1905a723d
VickyClaymore <vicky@claymoreusa.com>
2020-01-31 14:31:36 -0800

    fixed ea data record

--- ibet_apps/background/eaftp.py
+++ ibet_apps/background/eaftp.py
@@ -95,11 +95,17 @@ class GetEaBetHistory(View):
                                 # print(each_game_type)
                                 game_code = each_game_type['@code']
                                 bet_detail = each_game_type['deal']
+                                    
+                                # print(bet_detail)
                                 list_history = gameHistoryToDatabase(bet_detail, game_code)
                         # only one type of game playing in this time range
                         else:
                             game_code = all_game_types['game']['@code']
                             bet_detail = all_game_types['game']['deal']
+                            # if isinstance(bet_detail, list):
+                            #     bet_detail = each_game_type['deal']
+                            # else:
+                            print(bet_detail)
                             list_history = gameHistoryToDatabase(bet_detail, game_code)
                         gamebets_list = gamebets_list + list_history
                         logger.info('store EA bet history to database')
@@ -135,20 +141,21 @@ def gameHistoryToDatabase(bet_detail, game_code):
     except Exception as e:
         logger.critical("(FATAL__ERROR) There is missing EA provider or category. {}".format(str(e)))
 
-    for i in bet_detail:
-        game_code_id = i['@code']
-        game_end_date = i['@enddate']
-        game_id = i['@id']
-        game_start_date = i['@startdate']
-        game_status = i['@status']
-        bet_amount = i['betinfo']['clientbet']['@bet_amount']
-        payout_amount = i['betinfo']['clientbet']['@payout_amount']
-        username = i['betinfo']['clientbet']['@login']
-        valid_turnover = i['betinfo']['clientbet']['@valid_turnover']
+    if isinstance(bet_detail, dict):
+
+        game_code_id = bet_detail['@code']
+        game_end_date = bet_detail['@enddate']
+        game_id = bet_detail['@id']
+        game_start_date = bet_detail['@startdate']
+        game_status = bet_detail['@status']
+        bet_amount = bet_detail['betinfo']['clientbet']['@bet_amount']
+        payout_amount = bet_detail['betinfo']['clientbet']['@payout_amount']
+        username = bet_detail['betinfo']['clientbet']['@login']
+        valid_turnover = bet_detail['betinfo']['clientbet']['@valid_turnover']
 
         # print(game_code_id, game_id, game_end_date, game_start_date, game_status, bet_amount, payout_amount, username, valid_turnover)
 
-        # provider use Etc/GMT+8 timezone
+
         local = pytz.timezone ("Etc/GMT+8")
         game_start_date = datetime.datetime.strptime(game_start_date, "%Y-%m-%d %H:%M:%S")
         game_end_date = datetime.datetime.strptime(game_end_date, "%Y-%m-%d %H:%M:%S")
@@ -200,3 +207,71 @@ def gameHistoryToDatabase(bet_detail, game_code):
         bet_list.append(gamebet)
         logger.info("Successfully store the bet history to which code ID from EA is {}".format(game_code_id))
         return bet_list
+    
+    else:
+
+        for i in bet_detail:
+            game_code_id = i['@code']
+            game_end_date = i['@enddate']
+            game_id = i['@id']
+            game_start_date = i['@startdate']
+            game_status = i['@status']
+            bet_amount = i['betinfo']['clientbet']['@bet_amount']
+            payout_amount = i['betinfo']['clientbet']['@payout_amount']
+            username = i['betinfo']['clientbet']['@login']
+            valid_turnover = i['betinfo']['clientbet']['@valid_turnover']
+
+            # print(game_code_id, game_id, game_end_date, game_start_date, game_status, bet_amount, payout_amount, username, valid_turnover)
+
+            # provider use Etc/GMT+8 timezone
+            local = pytz.timezone ("Etc/GMT+8")
+            game_start_date = datetime.datetime.strptime(game_start_date, "%Y-%m-%d %H:%M:%S")
+            game_end_date = datetime.datetime.strptime(game_end_date, "%Y-%m-%d %H:%M:%S")
+            local_start_date = local.localize(game_start_date, is_dst=None)
+            local_end_date = local.localize(game_end_date, is_dst=None)
+            utc_start_date = local_start_date.astimezone(pytz.utc)
+            utc_end_date = local_end_date.astimezone(pytz.utc)
+
+            user = CustomUser.objects.get(username=username)
+            ibet_trans_id = user.username + "-" + timezone.datetime.today().isoformat() + "-" + str(random.randint(0, 10000000))
+            if int(bet_amount) > int(payout_amount):
+                outcome = 1
+            elif int(bet_amount) == int(payout_amount):
+                outcome = 2
+            else:
+                outcome = 0
+            
+            
+            gamebet = GameBet(
+                provider=provider,
+                category=category,
+                #game = None,
+                #game_name = None,
+                user=user,
+                user_name=user.username,
+                amount_wagered = float(int(bet_amount)/100),
+                amount_won = float(int(payout_amount)/100),
+                outcome = outcome,
+                #odds = None,
+                #bet_type = None,
+                #line = None,
+                transaction_id=ibet_trans_id,
+                currency=user.currency,
+                market=ibetVN, # do we need to store the market for game bet?
+                ref_no=game_code_id,
+                bet_time=utc_start_date,   #bet_time field should change
+                resolved_time=utc_end_date,
+                other_data={
+                    "start_time": str(game_start_date), 
+                    "end_time": str(game_end_date),
+                    "game_status": game_status,
+                    "valid_turnover": valid_turnover,
+                    "game_type": game_code,
+                    "deal_unique_id": game_id,
+                    "game_code_id": game_code_id
+                    }
+            )
+
+            bet_list.append(gamebet)
+            logger.info("Successfully store the bet history to which code ID from EA is {}".format(game_code_id))
+            return bet_list
